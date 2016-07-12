e0d67030f0252ff6beaf68ba9a6e3875f445cb61
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2016-07-12 15:05:17 +0100

    Made the script automatically create the output directory

--- Macros/Step1_ProcessnTuples.sh
+++ Macros/Step1_ProcessnTuples.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [[ ! -d ~/local-area/Stop4Body/New ]] ; then
+  mkdir -p ~/local-area/Stop4Body/New
+fi
+
 processSamples --json samples.json --outDir ~/local-area/Stop4Body/New/
 hadd ~/local-area/Stop4Body/New/Background_bdt.root ~/local-area/Stop4Body/New/Wjets_100to200_bdt.root ~/local-area/Stop4Body/New/Wjets_200to400_bdt.root ~/local-area/Stop4Body/New/Wjets_400to600_bdt.root ~/local-area/Stop4Body/New/Wjets_600toInf_bdt.root ~/local-area/Stop4Body/New/TTJets_LO_bdt.root
