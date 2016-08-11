e018f8a4c226f4749f4231de911786320f637251
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2016-08-11 16:16:44 +0100

    Changed to use variables for output dirs. Updated samples to newest ones

--- Macros/Step2_CreatePseudoData.sh
+++ Macros/Step2_CreatePseudoData.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+INPUT=~/local-area/Stop4Body/New/
+OUTPUT=~/local-area/Stop4Body/New/
+INPUT=~/local-area/Stop4Body/LepFix/
+OUTPUT=~/local-area/Stop4Body/LepFix/
+
 if [[ -d ~/local-area/Stop4Body/New ]] ; then
-  makePseudoData --json Macros/samples.json --inDir ~/local-area/Stop4Body/New/ --outDir ~/local-area/Stop4Body/New/ --lumi 5000 --noPresel
+  makePseudoData --json samplesInj.json --inDir ${INPUT} --outDir ${OUTPUT} --lumi 5000 --noPresel --injectSignal
+  mv ${OUTPUT}/PseudoData_bdt.root ${OUTPUT}/PseudoData_Injected_bdt.root
+  makePseudoData --json samples.json --inDir ${INPUT} --outDir ${OUTPUT} --lumi 5000 --noPresel
 fi
 
