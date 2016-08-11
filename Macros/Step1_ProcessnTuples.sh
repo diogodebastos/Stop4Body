c30a816e31487fe18ba224079140e2c552f62fef
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2016-08-11 18:16:47 +0200

    Updated step1 macro to use a variable

--- Macros/Step1_ProcessnTuples.sh
+++ Macros/Step1_ProcessnTuples.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [[ ! -d ~/local-area/Stop4Body/New ]] ; then
-  mkdir -p ~/local-area/Stop4Body/New
+OUTDIR=~/local-area/Stop4Body/New/
+
+if [[ ! -d ${OUTDIR} ]] ; then
+  mkdir -p ${OUTDIR}
 fi
 
-processSamples --json samples.json --outDir ~/local-area/Stop4Body/New/
-hadd ~/local-area/Stop4Body/New/Background_bdt.root ~/local-area/Stop4Body/New/Wjets_100to200_bdt.root ~/local-area/Stop4Body/New/Wjets_200to400_bdt.root ~/local-area/Stop4Body/New/Wjets_400to600_bdt.root ~/local-area/Stop4Body/New/Wjets_600toInf_bdt.root ~/local-area/Stop4Body/New/TTJets_LO_bdt.root
+processSamples --json samples.json --outDir ${OUTDIR}
+hadd ${OUTDIR}/Background_bdt.root ${OUTDIR}/Wjets_100to200_bdt.root ${OUTDIR}/Wjets_200to400_bdt.root ${OUTDIR}/Wjets_400to600_bdt.root ${OUTDIR}/Wjets_600toInf_bdt.root ${OUTDIR}/TTJets_LO_bdt.root
