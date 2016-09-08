d95835f1f0616a2e69ba705f422ba8ee387e4185
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2016-09-08 18:19:18 +0100

    Reverting to BDT

--- Macros/Step3_TrainMVA.sh
+++ Macros/Step3_TrainMVA.sh
@@ -4,8 +4,7 @@ SIGNAL=~/local-area/Stop4Body/nTuples_v2016-08-31/T2DegStop_300_270.root
 BACKGROUND=~/local-area/Stop4Body/nTuples_v2016-08-31/Background.root
 
 if [[ -f ${SIGNAL} && -f ${BACKGROUND} ]] ; then
-  #trainMVA --method BDT --signalFile ${SIGNAL} --backgroundFile ${BACKGROUND}
-  trainMVA --method NN --signalFile ${SIGNAL} --backgroundFile ${BACKGROUND}
+  trainMVA --method BDT --signalFile ${SIGNAL} --backgroundFile ${BACKGROUND}
 #  root runTMVAGui.C
   #DIR=$(pwd)
   #echo "Changing to TMVAGui directory"
