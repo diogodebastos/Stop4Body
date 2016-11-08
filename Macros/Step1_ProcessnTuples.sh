4604071de4d0519626593b5ab5e8d401b9072ba3
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2016-11-08 11:36:41 +0000

    Updates

--- Macros/Step1_ProcessnTuples.sh
+++ Macros/Step1_ProcessnTuples.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-OUTDIR=~cbeiraod/local-area/Stop4Body/nTuples_v2016-10-20/
+OUTDIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2016-11-07/
+#OUTDIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_synch/
 
 if [[ ! -d ${OUTDIR} ]] ; then
   mkdir -p ${OUTDIR}
 fi
 
+#processSamples --json synch.json --outDir ${OUTDIR}
 processSamples --json samples.json --outDir ${OUTDIR}
-hadd ${OUTDIR}/Background.root ${OUTDIR}/Wjets_100to200.root ${OUTDIR}/Wjets_200to400.root ${OUTDIR}/Wjets_400to600.root ${OUTDIR}/Wjets_600toInf.root ${OUTDIR}/TTJets.root
+#processSamples --json deltaM.json --outDir ${OUTDIR}
+hadd ${OUTDIR}/Background.root ${OUTDIR}/Wjets_100to200.root ${OUTDIR}/Wjets_200to400.root ${OUTDIR}/Wjets_400to600.root ${OUTDIR}/Wjets_600to800.root ${OUTDIR}/Wjets_800to1200.root ${OUTDIR}/Wjets_1200to2500.root ${OUTDIR}/Wjets_2500toInf.root ${OUTDIR}/TTJets.root
