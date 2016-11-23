53c11fb1e065ddb6ed306d57119e4613c1778b99
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2016-11-23 11:45:25 +0000

    Adding setup scripts

---
+++ Macros/setupPaths.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export BASE_PATH=$CMSSW_BASE/src/UserCode/Stop4Body/
+export JSON_PATH=$BASE_PATH/Macros/JSON/
+
+export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2016-11-23
+export SYNC_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_sync
+
+export TRAIN_DIR=${NTUPLE_DIR}_train
+export TEST_DIR=${NTUPLE_DIR}_test
+
+
