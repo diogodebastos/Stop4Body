d648da9276b4c869719d9d1697ac0e24776d51d9
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2016-11-29 11:29:07 +0000

    Added hadd of background file (for training MVA) for the not splitted samples

--- Macros/Step1_ProcessSamples_MCJob.sh
+++ Macros/Step1_ProcessSamples_MCJob.sh
@@ -17,6 +17,8 @@ processSamples --json $JSON_PATH/allMC.json --outDir ${NTUPLE_DIR}
 
 splitTrainingTest --json $JSON_PATH/allMC.json --inDir ${NTUPLE_DIR} --testOutDir ${TEST_DIR} --trainOutDir ${TRAIN_DIR}
 
+hadd ${NTUPLE_DIR}/Background_TTbar.root ${NTUPLE_DIR}/Wjets_100to200.root ${NTUPLE_DIR}/Wjets_200to400.root ${NTUPLE_DIR}/Wjets_400to600.root ${NTUPLE_DIR}/Wjets_600to800.root ${NTUPLE_DIR}/Wjets_800to1200.root ${NTUPLE_DIR}/Wjets_1200to2500.root ${NTUPLE_DIR}/Wjets_2500toInf.root ${NTUPLE_DIR}/TTJets.root
+hadd ${NTUPLE_DIR}/Background_TTbarLO.root ${NTUPLE_DIR}/Wjets_100to200.root ${NTUPLE_DIR}/Wjets_200to400.root ${NTUPLE_DIR}/Wjets_400to600.root ${NTUPLE_DIR}/Wjets_600to800.root ${NTUPLE_DIR}/Wjets_800to1200.root ${NTUPLE_DIR}/Wjets_1200to2500.root ${NTUPLE_DIR}/Wjets_2500toInf.root ${NTUPLE_DIR}/TTJets_LO.root
 hadd ${TEST_DIR}/Background_TTbar.root ${TEST_DIR}/Wjets_100to200.root ${TEST_DIR}/Wjets_200to400.root ${TEST_DIR}/Wjets_400to600.root ${TEST_DIR}/Wjets_600to800.root ${TEST_DIR}/Wjets_800to1200.root ${TEST_DIR}/Wjets_1200to2500.root ${TEST_DIR}/Wjets_2500toInf.root ${TEST_DIR}/TTJets.root
 hadd ${TEST_DIR}/Background_TTbarLO.root ${TEST_DIR}/Wjets_100to200.root ${TEST_DIR}/Wjets_200to400.root ${TEST_DIR}/Wjets_400to600.root ${TEST_DIR}/Wjets_600to800.root ${TEST_DIR}/Wjets_800to1200.root ${TEST_DIR}/Wjets_1200to2500.root ${TEST_DIR}/Wjets_2500toInf.root ${TEST_DIR}/TTJets_LO.root
 hadd ${TRAIN_DIR}/Background_TTbar.root ${TRAIN_DIR}/Wjets_100to200.root ${TRAIN_DIR}/Wjets_200to400.root ${TRAIN_DIR}/Wjets_400to600.root ${TRAIN_DIR}/Wjets_600to800.root ${TRAIN_DIR}/Wjets_800to1200.root ${TRAIN_DIR}/Wjets_1200to2500.root ${TRAIN_DIR}/Wjets_2500toInf.root ${TRAIN_DIR}/TTJets.root
