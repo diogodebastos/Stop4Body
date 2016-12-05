9edb5ffb2169570eb968e8363a59d2d1dd7de377
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2016-12-05 12:34:26 +0000

    Temporary change to get the full nTuples... the data nTuples will have to be linked into the train and test dirs...

--- Macros/Step5_GetYieldsAndPlots.sh
+++ Macros/Step5_GetYieldsAndPlots.sh
@@ -17,8 +17,8 @@ if [[ -d ${TEST_DIR} ]] ; then
     mkdir -p ${OUTPUT_LO}
   fi
 
-  getYields --json ${JSON_PATH}/plot2016B.json    --inDir ${TEST_DIR} --variables variables.json --outDir ${OUTPUT}    --lumi 5877
-  getYields --json ${JSON_PATH}/plot2016B_LO.json --inDir ${TEST_DIR} --variables variables.json --outDir ${OUTPUT_LO} --lumi 5877
+  getYields --json ${JSON_PATH}/plot2016B.json    --inDir ${NTUPLE_DIR} --variables variables.json --outDir ${OUTPUT}    --lumi 5877
+  getYields --json ${JSON_PATH}/plot2016B_LO.json --inDir ${NTUPLE_DIR} --variables variables.json --outDir ${OUTPUT_LO} --lumi 5877
   #getYields --json samples2Run.json --inDir ${INPUT} --variables variables.json --outDir ${OUTPUT_PD}
   #getYields --json samples2RunData.json --inDir ${INPUT} --variables variables.json --outDir ${OUTPUT_Data} --lumi 1700
 fi
