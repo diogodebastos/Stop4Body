8815acf11f09e02d2506a813aae7c47fa870d092
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2016-12-16 11:53:02 +0000

    Updated paths for latest samples

--- Macros/setupPaths.sh
+++ Macros/setupPaths.sh
@@ -3,13 +3,13 @@
 export BASE_PATH=$CMSSW_BASE/src/UserCode/Stop4Body/
 export JSON_PATH=$BASE_PATH/Macros/JSON/
 
-export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2016-12-05
-#export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2016-12-05_jet30
+#export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2016-12-12
+export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2016-12-12_jet30
 export SYNC_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_sync
 
 export TRAIN_DIR=${NTUPLE_DIR}_train
 export TEST_DIR=${NTUPLE_DIR}_test
 
 
-export PROCESS_SAMPLES_CMD="processSamples"
-#export PROCESS_SAMPLES_CMD="processSamples --jetThreshold30ForPreselection"
+#export PROCESS_SAMPLES_CMD="processSamples"
+export PROCESS_SAMPLES_CMD="processSamples --jetThreshold30ForPreselection"
