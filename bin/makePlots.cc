a5ddd96708ab9bd646b8aeafcea80c11c08f77af
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2016-12-16 15:05:14 +0000

    Started adding binary specifically to make plots

---
+++ bin/makePlots.cc
@@ -0,0 +1,138 @@
+#include <TROOT.h>
+#include <TFile.h>
+#include <TTree.h>
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <fstream>
+
+#include "UserCode/Stop4Body/interface/json.hpp"
+#include "UserCode/Stop4Body/interface/SampleReader.h"
+#include "UserCode/Stop4Body/interface/VariableReader.h"
+
+using json = nlohmann::json;
+
+void printHelp();
+
+class CutInfo
+{
+public:
+  CutInfo(std::string name, std::string cut, std::string latex): name_(name), cut_(cut), latex_(latex) {}
+
+private:
+protected:
+  std::string name_;
+  std::string cut_;
+  std::string latex_;
+};
+
+int main(int argc, char** argv)
+{
+  std::string jsonFileName = "";
+  std::string inputDirectory = "";
+  std::string outputDirectory = "./plots/";
+  std::string suffix = "";
+  std::string variablesJson = "";
+  double luminosity = -1.0;
+  bool isSplit = false;
+
+  if(argc < 2)
+  {
+    std::cout << "You did not pass enough parameters" << std::endl;
+    printHelp();
+    return 0;
+  }
+
+  for(int i = 1; i < argc; ++i)
+  {
+    std::string argument = argv[i];
+
+    if(argument == "--help")
+    {
+      printHelp();
+      return 0;
+    }
+
+    if(argument == "--json")
+      jsonFileName = argv[++i];
+
+    if(argument == "--outDir")
+      outputDirectory = argv[++i];
+
+    if(argument == "--inDir")
+      inputDirectory = argv[++i];
+
+    if(argument == "--suffix")
+      suffix = argv[++i];
+
+    if(argument == "--variables")
+      variablesJson = argv[++i];
+
+    if(argument == "--lumi")
+    {
+      std::stringstream convert;
+      convert << argv[++i];
+      convert >> luminosity;
+    }
+
+    if(argument == "--isSplit")
+    {
+      isSplit = true;
+    }
+  }
+
+  if(jsonFileName == "")
+  {
+    std::cout << "You must define a json file" << std::endl;
+    return 1;
+  }
+
+  if(variablesJson == "")
+  {
+    std::cout << "You must define a json file with the variables to plot" << std::endl;
+    return 1;
+  }
+
+  if(inputDirectory == "")
+  {
+    std::cout << "You must define an input directory" << std::endl;
+    return 1;
+  }
+
+  std::cout << "Reading json file" << std::endl;
+  SampleReader samples(jsonFileName, inputDirectory, suffix);
+
+  auto MC = samples.getMCBkg();
+  auto Sig = samples.getMCSig();
+  auto Data = samples.getData();
+
+  if(luminosity <= 0)
+    luminosity = Data.getLumi();
+
+  std::string mcWeight;
+  {
+    std::stringstream converter;
+    if(isSplit)
+      converter << "2*";
+    converter << "weight*" << luminosity;
+    converter >> mcWeight;
+  }
+
+  std::vector<CutInfo> cutFlow;
+  cutFlow.push_back(CutInfo("Preselection", "HT30 > 200", "$H_T > 300$"));
+  cutFlow.push_back(CutInfo("JetPt110", "Jet1Pt > 110", "$p_T\\left(j_1\\right) > 110$"));
+  cutFlow.push_back(CutInfo("MET300", "Met > 300", "$MET > 300$"));
+  cutFlow.push_back(CutInfo("LepPt30", "LepPt < 30", "$p_T\\left(l\\right) < 30$"));
+
+  return 0;
+}
+
+void printHelp()
+{
+  std::cout << "The program has the following parameters:" << std::endl;
+  std::cout << "\t--help\t- Print this message" << std::endl;
+
+  return;
+}
