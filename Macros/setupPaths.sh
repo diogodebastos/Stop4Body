d4c3a7ccde24e4eeb66406a24295ca0e22ae0af3
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2016-12-18 17:16:59 +0000

    Updated paths

--- Macros/setupPaths.sh
+++ Macros/setupPaths.sh
@@ -3,8 +3,8 @@
 export BASE_PATH=$CMSSW_BASE/src/UserCode/Stop4Body/
 export JSON_PATH=$BASE_PATH/Macros/JSON/
 
-#export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2016-12-12
-export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2016-12-12_jet30
+#export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2016-12-18
+export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2016-12-18_jet30
 export SYNC_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_sync
 
 export TRAIN_DIR=${NTUPLE_DIR}_train
