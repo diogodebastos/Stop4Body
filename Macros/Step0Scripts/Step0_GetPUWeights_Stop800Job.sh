98fc2e8e6e2d089e78ddf85da7cd8c7240325897
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2016-12-18 12:17:01 +0000

    Reorganized Step0

---
+++ Macros/Step0Scripts/Step0_GetPUWeights_Stop800Job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+alias cmsenv='eval `scramv1 runtime -sh`'
+
+cd /exper-sw/cmst3/cmssw/users/cbeiraod/
+. setup.sh
+
+#cd /exper-sw/cmst3/cmssw/users/cbeiraod/CMSSW_8_0_14/src/
+cd $CMSSW_BASE/src/
+eval `scramv1 runtime -sh`
+
+#cd /exper-sw/cmst3/cmssw/users/cbeiraod/CMSSW_8_0_14/src/UserCode/Stop4Body/Macros/
+cd UserCode/Stop4Body/Macros/
+
+. setupPaths.sh
+
+if [[ -d ${NTUPLE_DIR} ]]; then
+  getPUWeight --json ${JSON_PATH}/stop800.json  --outDir ${NTUPLE_DIR} --dataPU DataPileupHistogram.root
+fi
+
--- Macros/runStep0.sh
+++ Macros/runStep0.sh
@@ -7,36 +7,38 @@ if [[ ! -d ${NTUPLE_DIR} ]] ; then
   mkdir -p ${NTUPLE_DIR}
 fi
 
+cd Step0Scripts
+
 qsub Step0_GetPUWeights.sh
 
 # Submit job for all MC
-#qsub Step1_ProcessSamples_backgroundMCJob.sh
-#qsub Step1_ProcessSamples_otherMCJob.sh
+qsub Step0_GetPUWeights_backgroundMCJob.sh
+qsub Step0_GetPUWeights_otherMCJob.sh
 
 # Submit job for all Data
-#qsub Step1_ProcessSamples_DataJob.sh
+qsub Step1_ProcessSamples_DataJob.sh
 
 # Submit job for each group of signal points with a given stop mass
-#qsub Step1_ProcessSamples_Stop250Job.sh
-#qsub Step1_ProcessSamples_Stop275Job.sh
-#qsub Step1_ProcessSamples_Stop300Job.sh
-#qsub Step1_ProcessSamples_Stop325Job.sh
-#qsub Step1_ProcessSamples_Stop350Job.sh
-#qsub Step1_ProcessSamples_Stop375Job.sh
-#qsub Step1_ProcessSamples_Stop400Job.sh
-#qsub Step1_ProcessSamples_Stop425Job.sh
-#qsub Step1_ProcessSamples_Stop450Job.sh
-#qsub Step1_ProcessSamples_Stop475Job.sh
-#qsub Step1_ProcessSamples_Stop500Job.sh
-#qsub Step1_ProcessSamples_Stop525Job.sh
-#qsub Step1_ProcessSamples_Stop550Job.sh
-#qsub Step1_ProcessSamples_Stop575Job.sh
-#qsub Step1_ProcessSamples_Stop600Job.sh
-#qsub Step1_ProcessSamples_Stop625Job.sh
-#qsub Step1_ProcessSamples_Stop650Job.sh
-#qsub Step1_ProcessSamples_Stop675Job.sh
-#qsub Step1_ProcessSamples_Stop700Job.sh
-#qsub Step1_ProcessSamples_Stop725Job.sh
-#qsub Step1_ProcessSamples_Stop750Job.sh
-#qsub Step1_ProcessSamples_Stop775Job.sh
-#qsub Step1_ProcessSamples_Stop800Job.sh
+qsub Step0_GetPUWeights_Stop250Job.sh
+qsub Step0_GetPUWeights_Stop275Job.sh
+qsub Step0_GetPUWeights_Stop300Job.sh
+qsub Step0_GetPUWeights_Stop325Job.sh
+qsub Step0_GetPUWeights_Stop350Job.sh
+qsub Step0_GetPUWeights_Stop375Job.sh
+qsub Step0_GetPUWeights_Stop400Job.sh
+qsub Step0_GetPUWeights_Stop425Job.sh
+qsub Step0_GetPUWeights_Stop450Job.sh
+qsub Step0_GetPUWeights_Stop475Job.sh
+qsub Step0_GetPUWeights_Stop500Job.sh
+qsub Step0_GetPUWeights_Stop525Job.sh
+qsub Step0_GetPUWeights_Stop550Job.sh
+qsub Step0_GetPUWeights_Stop575Job.sh
+qsub Step0_GetPUWeights_Stop600Job.sh
+qsub Step0_GetPUWeights_Stop625Job.sh
+qsub Step0_GetPUWeights_Stop650Job.sh
+qsub Step0_GetPUWeights_Stop675Job.sh
+qsub Step0_GetPUWeights_Stop700Job.sh
+qsub Step0_GetPUWeights_Stop725Job.sh
+qsub Step0_GetPUWeights_Stop750Job.sh
+qsub Step0_GetPUWeights_Stop775Job.sh
+qsub Step0_GetPUWeights_Stop800Job.sh
