ad1760212b2c9d3a095746c2c8229fef49ea2bcd
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-01-11 11:59:08 +0000

    Updated MVA scripts to use the new setup scripts

--- Macros/Step4_ApplyMVA.sh
+++ Macros/Step4_ApplyMVA.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-INPUT=~cbeiraod/local-area/Stop4Body/nTuples_v2016-08-31/
-OUTPUT=~cbeiraod/local-area/Stop4Body/nTuples_v2016-08-31_bdt/
+. setupJSONs.sh
+. setupPaths.sh
+
+#INPUT=${NTUPLE_DIR}
+#INPUT=${TRAIN_DIR}
+INPUT=${TEST_DIR}
+OUTPUT=${INPUT}_bdt
 
 if [[ -d ${INPUT} ]] ; then
   if [[ ! -d ${OUTPUT} ]] ; then
     mkdir -p ${OUTPUT}
   fi
 
-  applyMVA --json samples.json --method BDT --inDir ${INPUT} --outDir ${OUTPUT}
+  applyMVA --json ${JSON_PATH}/allSamples.json --method BDT --inDir ${INPUT}/ --outDir ${OUTPUT}/
 fi
 
