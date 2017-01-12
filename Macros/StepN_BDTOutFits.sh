2fbe7aa1d89430de83f61e807be745d5ff8d50f0
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2017-01-12 19:24:57 +0000

    Updated StepN to use the setup scripts

--- Macros/StepN_BDTOutFits.sh
+++ Macros/StepN_BDTOutFits.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-INPUT=~cbeiraod/local-area/Stop4Body/nTuples_v2016-08-31_bdt/
+. setupJSONs.sh
+. setupPaths.sh
+
+INPUT=${TEST_DIR}_bdt/
 OUTPUT=~cbeiraod/local-area/Stop4Body/BDT_Fits/
 OUTPUT_INJ=~cbeiraod/local-area/Stop4Body/BDT_Fits_Injected/
 OUTPUT_DAT=~cbeiraod/local-area/Stop4Body/BDT_Fits_Data/
@@ -17,6 +20,6 @@ if [[ ! -d ${OUTPUT_DAT} ]] ; then
   mkdir -p ${OUTPUT_DAT}
 fi
 
-BDTStudy --json samplesInj2Run.json --outDir ${OUTPUT_INJ} --inDir ${INPUT} --suffix bdt
-BDTStudy --json samples2Run.json --outDir ${OUTPUT} --inDir ${INPUT} --suffix bdt
-BDTStudy --json samples2RunData.json --outDir ${OUTPUT_DAT} --inDir ${INPUT} --suffix bdt --lumi 1700
+BDTStudy --json ${JSON_PATH}/plot2016_PDInj_LO.json --outDir ${OUTPUT_INJ} --inDir ${INPUT} --suffix bdt
+BDTStudy --json ${JSON_PATH}/plot2016_PD_LO.json --outDir ${OUTPUT} --inDir ${INPUT} --suffix bdt
+BDTStudy --json ${JSON_PATH}/plot2016_LO.json --outDir ${OUTPUT_DAT} --inDir ${INPUT} --suffix bdt
