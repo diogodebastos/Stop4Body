c38d601d455d08d6172352296ec6682031528699
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2017-01-12 19:26:47 +0000

    Modified PU calculation

--- Macros/getPUJSONs.sh
+++ Macros/getPUJSONs.sh
@@ -3,9 +3,12 @@
 # Info at: https://twiki.cern.ch/twiki/bin/view/CMS/PdmV2016Analysis
 # Take from: https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/
 GOLDEN_JSON=https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/Cert_271036-277933_13TeV_PromptReco_Collisions16_JSON.txt
+PILEUP_LATEST=https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/PileUp/pileup_latest.txt
 
-scp cbeiraod@lxplus.cern.ch:/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt ./
+#scp cbeiraod@lxplus.cern.ch:/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt ./
+wget ${PILEUP_LATEST} -O pileup_latest.txt
 wget ${GOLDEN_JSON} -O GOLDEN_JSON.txt
 
 # Details: https://twiki.cern.ch/twiki/bin/viewauth/CMS/PileupJSONFileforData
 pileupCalc.py -i GOLDEN_JSON.txt --inputLumiJSON pileup_latest.txt  --calcMode true --minBiasXsec 69200 --maxPileupBin 100 --numPileupBins 100 DataPileupHistogram.root
+#pileupCalc.py -i GOLDEN_JSON.txt --inputLumiJSON pileup_latest.txt  --calcMode observed --minBiasXsec 69200 --maxPileupBin 100 --numPileupBins 100 DataPileupHistogram.root
