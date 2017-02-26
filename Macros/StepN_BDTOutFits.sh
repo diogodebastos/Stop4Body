9f34ccc4b5699528cc946ed5bb753b0d53791eee
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2017-02-26 13:07:43 +0000

    Reordered

--- Macros/StepN_BDTOutFits.sh
+++ Macros/StepN_BDTOutFits.sh
@@ -20,6 +20,6 @@ if [[ ! -d ${OUTPUT_DAT} ]] ; then
   mkdir -p ${OUTPUT_DAT}
 fi
 
+BDTStudy --json ${JSON_PATH}/plot2016_LO.json --outDir ${OUTPUT_DAT} --inDir ${INPUT} --suffix bdt
 BDTStudy --json ${JSON_PATH}/plot2016_PDInj_LO.json --outDir ${OUTPUT_INJ} --inDir ${INPUT} --suffix bdt
 BDTStudy --json ${JSON_PATH}/plot2016_PD_LO.json --outDir ${OUTPUT} --inDir ${INPUT} --suffix bdt
-BDTStudy --json ${JSON_PATH}/plot2016_LO.json --outDir ${OUTPUT_DAT} --inDir ${INPUT} --suffix bdt
