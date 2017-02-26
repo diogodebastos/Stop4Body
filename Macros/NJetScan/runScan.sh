1143eba5fe10d34d635db4a4705714816d7971ff
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-02-26 14:27:47 +0000

    no message

--- Macros/NJetScan/runScan.sh
+++ Macros/NJetScan/runScan.sh
@@ -10,4 +10,4 @@ if [[ ! -d ${PLOT_DIR} ]] ; then
   mkdir -p ${PLOT_DIR}
 fi
 
-makePlots --json ${SAMPLE_JSON} --outDir ${PLOT_DIR} --inDir ~/local-area/Stop4Body/nTuples_v2017-02-09/ --variables scan.json --cuts scan.json
+makePlots --json ${SAMPLE_JSON} --outDir ${PLOT_DIR} --inDir ${NTUPLE_DIR} --variables scan.json --cuts scan.json
