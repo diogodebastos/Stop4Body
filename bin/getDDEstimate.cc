20a5f708f308a3d4aedf25bc3e862e6bdab1c81a
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-03-13 15:33:48 +0000

    Updating DD estimate cuts

--- bin/getDDEstimate.cc
+++ bin/getDDEstimate.cc
@@ -35,10 +35,10 @@ int main(int argc, char** argv)
   bool isPseudoData = false;
   bool verbose = false;
 
-  std::string baseSelection = "(HT30 > 200) && (Jet1Pt > 100) && (Met > 300) && (nGoodEl + nGoodMu <= 2)";
+  std::string baseSelection = "(HT30 > 200) && (Jet1Pt > 110) && (Met > 280) && (nGoodEl + nGoodMu <= 2) && (LepPt < 30.)";
   std::string wjetsControlRegion = "(BDT < -0.1) && (NbLoose30 == 0)";
   std::string ttbarControlRegion = "(BDT < -0.1) && (NbTight30 > 0)";
-  std::string signalRegion       = "(BDT > 0.37)";
+  std::string signalRegion       = "(BDT > 0.38)";
 
   if(argc < 2)
   {
