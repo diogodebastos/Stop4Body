fa4a33a8aa1d0e20866c8c00f97118fbe0d7fd94
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-03-13 15:04:22 +0000

    Updated setup script for new production

--- Macros/setupPaths.sh
+++ Macros/setupPaths.sh
@@ -3,7 +3,7 @@
 export BASE_PATH=$CMSSW_BASE/src/UserCode/Stop4Body/
 export JSON_PATH=$BASE_PATH/Macros/JSON/
 
-export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2017-02-25
+export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2017-03-13
 export SYNC_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_sync
 export PU_TEST_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_PU_test
 
@@ -11,6 +11,5 @@ export TRAIN_DIR=${NTUPLE_DIR}_train
 export TEST_DIR=${NTUPLE_DIR}_test
 
 
-#export PROCESS_SAMPLES_CMD="processSamples"
-export PROCESS_SAMPLES_CMD="processSamples --jetThreshold30ForPreselection --doAltPU"
-export PROCESS_SAMPLES_CMD="processSamples --jetThreshold30ForPreselection"
+export PROCESS_SAMPLES_CMD="processSamples --jetPtThreshold 20"
+export PROCESS_SAMPLES_CMD="processSamples"
