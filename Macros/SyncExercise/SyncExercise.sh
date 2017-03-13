34878ac94fe21562f461508118523a7b79105980
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2017-03-13 15:06:27 +0000

    Removed old code references

--- Macros/SyncExercise/SyncExercise.sh
+++ Macros/SyncExercise/SyncExercise.sh
@@ -5,11 +5,9 @@ alias cmsenv='eval `scramv1 runtime -sh`'
 cd /exper-sw/cmst3/cmssw/users/cbeiraod/
 . setup.sh
 
-#cd /exper-sw/cmst3/cmssw/users/cbeiraod/CMSSW_8_0_14/src/
 cd $CMSSW_BASE/src/
 eval `scramv1 runtime -sh`
 
-#cd /exper-sw/cmst3/cmssw/users/cbeiraod/CMSSW_8_0_14/src/UserCode/Stop4Body/Macros/
 cd UserCode/Stop4Body/Macros/
 
 . setupPaths.sh
