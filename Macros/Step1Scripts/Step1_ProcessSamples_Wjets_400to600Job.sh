66b12ff81f25d8f8673542f4e7648cfac7cc0482
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-03-27 17:37:37 +0100

    Fixed ordering

--- Macros/Step1Scripts/Step1_ProcessSamples_Wjets_400to600Job.sh
+++ Macros/Step1Scripts/Step1_ProcessSamples_Wjets_400to600Job.sh
@@ -12,9 +12,8 @@ eval `scramv1 runtime -sh`
 #cd /exper-sw/cmst3/cmssw/users/cbeiraod/CMSSW_8_0_14/src/UserCode/Stop4Body/Macros/
 cd UserCode/Stop4Body/Macros/
 
-JSONFILE=$JSON_PATH/Orig/Wjets_400to600.json
-
 . setupPaths.sh
+JSONFILE=$JSON_PATH/Orig/Wjets_400to600.json
 MY_PROCESS_SAMPLES_CMD="$PROCESS_SAMPLES_CMD --json $JSONFILE --outDir ${NTUPLE_DIR}"
 eval $MY_PROCESS_SAMPLES_CMD
 
