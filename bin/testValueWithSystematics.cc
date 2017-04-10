ab210fcba52d3747807ee230560674e97d680d64
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-04-10 15:10:17 +0100

    Added test file for ValueWithSystematics

---
+++ bin/testValueWithSystematics.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "UserCode/Stop4Body/interface/ValueWithSystematics.h"
+
+int main(int argc, char** argv)
+{
+  std::cout << "This is a test of the ValueWithSystematics class" << std::endl;
+
+  return 0;
+}
