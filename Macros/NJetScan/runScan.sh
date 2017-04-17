94509108fd819bd1abc530e7efa6488fbb2ddffb
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-04-17 16:43:28 +0100

    Updated sample file for scan

--- Macros/NJetScan/runScan.sh
+++ Macros/NJetScan/runScan.sh
@@ -4,7 +4,7 @@
 . ../setupPaths.sh
 
 PLOT_DIR=./scanPlots
-SAMPLE_JSON=${JSON_PATH}/plot2016BCD.json
+SAMPLE_JSON=${JSON_PATH}/plot2016.json
 
 if [[ ! -d ${PLOT_DIR} ]] ; then
   mkdir -p ${PLOT_DIR}
