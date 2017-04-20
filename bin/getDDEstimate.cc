eb9b3111b31d75af6c608070e0192be1576105f5
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-04-20 09:32:22 +0100

    Missed two variables

--- bin/getDDEstimate.cc
+++ bin/getDDEstimate.cc
@@ -36,8 +36,8 @@ int main(int argc, char** argv)
   bool verbose = false;
 
   std::string baseSelection = "(HT > 200) && (Jet1Pt > 110) && (Met > 280) && (nGoodEl + nGoodMu <= 2) && (LepPt < 30.)";
-  std::string wjetsControlRegion = "(BDT < -0.1) && (NbLoose30 == 0)";
-  std::string ttbarControlRegion = "(BDT < -0.1) && (NbTight30 > 0)";
+  std::string wjetsControlRegion = "(BDT < -0.1) && (NbLoose == 0)";
+  std::string ttbarControlRegion = "(BDT < -0.1) && (NbTight > 0)";
   std::string signalRegion       = "(BDT > 0.4)";
 
   if(argc < 2)
