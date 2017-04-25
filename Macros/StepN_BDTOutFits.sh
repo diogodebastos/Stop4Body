8cea9a2c423b3cadfa22ef7bc39291d57223feb0
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-04-25 16:54:00 +0100

    Added swap VR to the BDT fits

--- Macros/StepN_BDTOutFits.sh
+++ Macros/StepN_BDTOutFits.sh
@@ -4,9 +4,13 @@
 . setupPaths.sh
 
 INPUT=${TEST_DIR}_bdt/
+INPUTS=${SWAP_DIR}_bdt/
 OUTPUT=~cbeiraod/local-area/Stop4Body/BDT_Fits/
 OUTPUT_POW=~cbeiraod/local-area/Stop4Body/BDT_Fits_pow/
 OUTPUT_LEP=~cbeiraod/local-area/Stop4Body/BDT_Fits_lep/
+OUTPUTS=~cbeiraod/local-area/Stop4Body/BDT_Fits_Swap/
+OUTPUTS_POW=~cbeiraod/local-area/Stop4Body/BDT_Fits_Swap_pow/
+OUTPUTS_LEP=~cbeiraod/local-area/Stop4Body/BDT_Fits_Swap_lep/
 
 if [[ ! -d ${OUTPUT} ]] ; then
   mkdir -p ${OUTPUT}
@@ -17,7 +21,20 @@ fi
 if [[ ! -d ${OUTPUT_LEP} ]] ; then
   mkdir -p ${OUTPUT_LEP}
 fi
+if [[ ! -d ${OUTPUTS} ]] ; then
+  mkdir -p ${OUTPUTS}
+fi
+if [[ ! -d ${OUTPUTS_POW} ]] ; then
+  mkdir -p ${OUTPUTS_POW}
+fi
+if [[ ! -d ${OUTPUTS_LEP} ]] ; then
+  mkdir -p ${OUTPUTS_LEP}
+fi
 
 BDTStudy --json ${JSON_PATH}/plot2016.json     --outDir ${OUTPUT}     --inDir ${INPUT} --suffix bdt
 BDTStudy --json ${JSON_PATH}/plot2016_pow.json --outDir ${OUTPUT_POW} --inDir ${INPUT} --suffix bdt
 BDTStudy --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT_LEP} --inDir ${INPUT} --suffix bdt
+
+BDTStudy --json ${JSON_PATH}/plot2016swap.json     --outDir ${OUTPUTS}     --inDir ${INPUTS} --suffix bdt
+BDTStudy --json ${JSON_PATH}/plot2016swap_pow.json --outDir ${OUTPUTS_POW} --inDir ${INPUTS} --suffix bdt
+BDTStudy --json ${JSON_PATH}/plot2016swap_lep.json --outDir ${OUTPUTS_LEP} --inDir ${INPUTS} --suffix bdt
