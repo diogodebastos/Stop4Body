ac28256aefb10233a24985d52f649956d4ee2661
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-04-27 16:34:34 +0100

    Moved SF functions

--- interface/commonFunctions.h
+++ interface/commonFunctions.h
@@ -6,6 +6,14 @@
 bool fileExists(std::string);
 float DeltaPhi(double, double);
 
+doubleUnc triggerEfficiencyFromMET(double);
+doubleUnc WISRScaleFactorFromLepMet(double lep_pt, double lep_phi, double met_pt, double met_phi);
+doubleUnc ISRweightFromNISRJet(int nISRJet);
+doubleUnc EWKISRweightFromISRpT(double ISRpT);
+doubleUnc EWKISRweightFromISRpT(double lep_pt, double lep_phi, double met_pt, double met_phi);
+doubleUnc getLeptonIDSF(double LepID, double LepPt, double LepEta);
+doubleUnc getLeptonISOSF(double LepID, double LepPt, double LepEta);
+
 doubleUnc stopCrossSection(double stopM, double lspM);
 
 #endif
