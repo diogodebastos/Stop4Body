59820589a0237e0bb4fb37aa617b590032f50f47
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-05-02 18:18:35 +0100

    Modified weight computation to grap previously computed values

--- bin/processSamples.cc
+++ bin/processSamples.cc
@@ -331,7 +331,6 @@ int main(int argc, char** argv)
 
       // Get total number of entries and other important values
       Nevt = 0;
-      double sumGenWeightCounting = 0;
       Ncut0 = 0;
       Ncut1 = 0;
       Ncut2 = 0;
@@ -343,107 +342,21 @@ int main(int argc, char** argv)
       double EWKISRCParam = 1;
       if(!process.isdata()) // Only perform the below for MC
       {
-        int nIsrBin[7]{0, 0, 0, 0, 0, 0, 0};
-        int EWKpTBin[8]{0, 0, 0, 0, 0, 0, 0, 0};
         std::cout << "\t  Getting Initial number of events, nvtx distribution and sum of gen weights: " << std::flush;
-        for(auto &file : sample)
-        {
-          TFile finput(file.c_str(), "READ");
-          foutput.cd();
-          TTree *inputtree;
-          if(process.selection() != "")
-            inputtree = static_cast<TTree*>(finput.Get("tree"))->CopyTree(process.selection().c_str());
-          else
-            inputtree = static_cast<TTree*>(finput.Get("tree"));
 
-          size_t thisNevt = static_cast<size_t>(inputtree->GetEntries());
-          Nevt += thisNevt;
+        TVectorD* sampleNevt = static_cast<TVectorD*>(puWeightFile.Get(("sample_"+sample.tag()+"_Nevt").c_str())->Clone("sampleNevt"));
+        Nevt = (*sampleNevt)[0];
+        TVectorD* sampleSumGenWeight = static_cast<TVectorD*>(puWeightFile.Get(("sample_"+sample.tag()+"_sumGenWeight").c_str())->Clone("sampleSumGenWeight"));
+        sumGenWeight = (*sampleSumGenWeight)[0];
+        TVectorD* processISRWeights = static_cast<TVectorD*>(puWeightFile.Get()->Clone("processISRWeights"));
+        ISRCParam =    (*processISRWeights)[0];
+        EWKISRCParam = (*processISRWeights)[1];
 
-          Float_t thisGenWeight = 0;
-          inputtree->SetBranchAddress("genWeight", &thisGenWeight);
-          Float_t nIsr; inputtree->SetBranchAddress("nIsr", &nIsr);
-          Float_t met_pt;      inputtree->SetBranchAddress("met_pt"    , &met_pt);
-          Float_t met_phi;     inputtree->SetBranchAddress("met_phi",   &met_phi);
-          Int_t nLepGood;      inputtree->SetBranchAddress("nLepGood"   , &nLepGood);
-          Float_t LepGood_pt[LEPCOLL_LIMIT];  inputtree->SetBranchAddress("LepGood_pt", &LepGood_pt);
-          Float_t LepGood_phi[LEPCOLL_LIMIT];  inputtree->SetBranchAddress("LepGood_phi", &LepGood_phi);
-          double smallCounter = 0;
-          for(size_t i = 0; i < thisNevt; ++i)
-          {
-            inputtree->GetEntry(i);
+        delete sampleNevt;
+        delete sampleSumGenWeight;
+        delete processISRWeights;
 
-            smallCounter += thisGenWeight;
-
-            Int_t nIsr_switch = nIsr;
-            if(nIsr_switch > 6)
-              nIsr_switch = 6;
-            nIsrBin[nIsr_switch]++;
-
-            if(nLepGood > 0)
-            {
-              double lep_x = LepGood_pt[0] * std::cos(LepGood_phi[0]);
-              double lep_y = LepGood_pt[0] * std::sin(LepGood_phi[0]);
-              double met_x = met_pt * std::cos(met_phi);
-              double met_y = met_pt * std::sin(met_phi);
-
-              double w_pt = std::sqrt((lep_x + met_x)*(lep_x + met_x) + (lep_y + met_y)*(lep_y + met_y));
-
-              int EWKindex = 7;
-              if(w_pt < 600)
-                EWKindex = 6;
-              if(w_pt < 400)
-                EWKindex = 5;
-              if(w_pt < 300)
-                EWKindex = 4;
-              if(w_pt < 200)
-                EWKindex = 3;
-              if(w_pt < 150)
-                EWKindex = 2;
-              if(w_pt < 100)
-                EWKindex = 1;
-              if(w_pt < 50)
-                EWKindex = 0;
-
-              EWKpTBin[EWKindex]++;
-            }
-          }
-          sumGenWeightCounting += smallCounter;
-
-          if(process.selection() != "")
-            delete inputtree;
-        }
-        sumGenWeight = sumGenWeightCounting; // Consider implementing the streaming float summation: http://dl.acm.org/citation.cfm?id=1824815
         std::cout << Nevt << "; " << sumGenWeight << std::endl;
-        ISRCParam = (                               nIsrBin[0] +
-                                                    nIsrBin[1] +
-                                                    nIsrBin[2] +
-                                                    nIsrBin[3] +
-                                                    nIsrBin[4] +
-                                                    nIsrBin[5] +
-                                                    nIsrBin[6]  ) /
-        static_cast<double>(ISRweightFromNISRJet(0)*nIsrBin[0] +
-                            ISRweightFromNISRJet(1)*nIsrBin[1] +
-                            ISRweightFromNISRJet(2)*nIsrBin[2] +
-                            ISRweightFromNISRJet(3)*nIsrBin[3] +
-                            ISRweightFromNISRJet(4)*nIsrBin[4] +
-                            ISRweightFromNISRJet(5)*nIsrBin[5] +
-                            ISRweightFromNISRJet(6)*nIsrBin[6]  );
-        EWKISRCParam = (                                 EWKpTBin[0] +
-                                                         EWKpTBin[1] +
-                                                         EWKpTBin[2] +
-                                                         EWKpTBin[3] +
-                                                         EWKpTBin[4] +
-                                                         EWKpTBin[5] +
-                                                         EWKpTBin[6] +
-                                                         EWKpTBin[7]  ) /
-        static_cast<double>(EWKISRweightFromISRpT( 25.0)*EWKpTBin[0] +
-                            EWKISRweightFromISRpT( 75.0)*EWKpTBin[1] +
-                            EWKISRweightFromISRpT(125.0)*EWKpTBin[2] +
-                            EWKISRweightFromISRpT(175.0)*EWKpTBin[3] +
-                            EWKISRweightFromISRpT(250.0)*EWKpTBin[4] +
-                            EWKISRweightFromISRpT(350.0)*EWKpTBin[5] +
-                            EWKISRweightFromISRpT(500.0)*EWKpTBin[6] +
-                            EWKISRweightFromISRpT(700.0)*EWKpTBin[7]  );
         std::cout << "\t  C value for ISR reweighting: " << ISRCParam << std::endl;
         std::cout << "\t  C value for EWK ISR reweighting: " << EWKISRCParam << std::endl;
       }
