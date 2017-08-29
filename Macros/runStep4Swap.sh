7247a6e8bff492616ab68234d4f3111f2e37c6e9
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-08-29 11:52:26 +0100

    Added apply step for swap

---
+++ Macros/runStep4Swap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+. setupJSONs.sh
+. setupPaths.sh
+
+CURRENT_BDT=$1
+
+#for DIR in "${NTUPLE_DIR}" "${TEST_DIR}" ;do
+  DIR=${SWAP_DIR}
+  echo "Processing directory: ${DIR}"
+  OUTPUT=${DIR}_bdt${CURRENT_BDT}
+
+  if [[ ! -d ${OUTPUT} ]] ; then
+    mkdir -p ${OUTPUT}
+  fi
+
+  python applyMVA.py -i ${DIR} -o ${OUTPUT} -w weights_bdt${CURRENT_BDT}
+#done
+
+echo "Run the following commands to check the progress of the output and resubmit failed jobs:"
+echo "  python checkJobs.py -o ${SWAP_DIR}_bdt${CURRENT_BDT}"
+#echo "  python checkJobs.py -o ${TEST_DIR}_bdt${CURRENT_BDT}"
