672a46627d0d252c2052ab677f4ec7620390f583
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-08-29 11:40:02 +0100

    Updated AN plotter

--- Macros/makePlotsAN.sh
+++ Macros/makePlotsAN.sh
@@ -4,34 +4,59 @@
 . setupPaths.sh
 
 INPUT=~/local-area/Stop4Body/nTuples_v2017-06-05_SysVar
-INPUTS=~/local-area/Stop4Body/nTuples_v2017-08-13_swap
+INPUT_TEST=~/local-area/Stop4Body/nTuples_v2017-06-05_SysVar_test
+INPUT_SWAP=~/local-area/Stop4Body/nTuples_v2017-08-13_swap
 OUTPUT=~/local-area/Stop4Body/ANPlots/
 #OUTPUT=./test/
 
 if [[ -d ${INPUT} ]] ; then
   if [[ ! -d ${OUTPUT} ]] ; then
-    mkdir -p ${OUTPUT}/DataMC
-    mkdir -p ${OUTPUT}/DataMC_swap
+    mkdir -p ${OUTPUT}/DataMC/AR
+    mkdir -p ${OUTPUT}/DataMC/LNT
+    mkdir -p ${OUTPUT}/DataMC/VR1
+    mkdir -p ${OUTPUT}/DataMC/VR2
+    mkdir -p ${OUTPUT}/DataMC/VR3
   fi
 
-  JSONFILE=variablesAN_DataMC.json
-  #makePlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/DataMC/ --inDir ${INPUT}_test/ --variables ${JSONFILE} --cuts ${JSONFILE}
+  VARIABLE_JSON=variablesAN_DataMC.json
 
-  JSONFILE=variablesAN_DataMC_swap.json
-  #makePlots --json ${JSON_PATH}/plot2016swap_lep.json --outDir ${OUTPUT}/DataMC_swap/ --inDir ${INPUTS}/ --variables ${JSONFILE} --cuts ${JSONFILE}
+  JSONFILE=variablesAN_DataMC.json
+  makePlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/DataMC/AR/ --inDir ${INPUT_TEST}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
+
+  JSONFILE=variablesAN_DataMC_LNT.json
+  makePlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/DataMC/LNT/ --inDir ${INPUT}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
+
+  JSONFILE=variablesAN_DataMC_VR1.json
+  makePlots --json ${JSON_PATH}/plot2016swap_lep.json --outDir ${OUTPUT}/DataMC/VR1/ --inDir ${INPUT_SWAP}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
+
+  JSONFILE=variablesAN_DataMC_VR2.json
+  makePlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/DataMC/VR2/ --inDir ${INPUT}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
+
+  JSONFILE=variablesAN_DataMC_VR3.json
+  makePlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/DataMC/VR3/ --inDir ${INPUT}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
 
   for deltaM in 10 20 30 40 50 60 70 80; do
-  #for deltaM in 50; do
     mkdir -p ${OUTPUT}/BDT/DeltaM${deltaM}/AR
+    mkdir -p ${OUTPUT}/BDT/DeltaM${deltaM}/LNT
     mkdir -p ${OUTPUT}/BDT/DeltaM${deltaM}/VR1
     mkdir -p ${OUTPUT}/BDT/DeltaM${deltaM}/VR2
+    mkdir -p ${OUTPUT}/BDT/DeltaM${deltaM}/VR3
 
-    JSONFILE=variablesAN_BDT_SR.json
-    #makePlots --json ${JSON_PATH}/plot2016_DM${deltaM}RP.json --outDir ${OUTPUT}/BDT/DeltaM${deltaM}/AR --inDir ${INPUT}_test_bdt${deltaM}/ --variables ${JSONFILE} --cuts ${JSONFILE} --suffix bdt
+    VARIABLE_JSON=variablesAN_BDT_AR.json
+
+    JSONFILE=variablesAN_BDT_AR.json
+    makePlots --json ${JSON_PATH}/plot2016_DM${deltaM}RP.json --outDir ${OUTPUT}/BDT/DeltaM${deltaM}/AR --inDir ${INPUT_TEST}_bdt${deltaM}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE} --suffix bdt
+
+    JSONFILE=variablesAN_BDT_LNT.json
+    makePlots --json ${JSON_PATH}/plot2016_DM${deltaM}RP.json --outDir ${OUTPUT}/BDT/DeltaM${deltaM}/LNT --inDir ${INPUT}_bdt${deltaM}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE} --suffix bdt --unblind
+
     JSONFILE=variablesAN_BDT_VR1.json
-    #makePlots --json ${JSON_PATH}/plot2016_DM${deltaM}RP.json --outDir ${OUTPUT}/BDT/DeltaM${deltaM}/VR2 --inDir ${INPUT}_bdt${deltaM}/ --variables ${JSONFILE} --cuts ${JSONFILE} --suffix bdt --unblind
+    makePlots --json ${JSON_PATH}/plot2016swap_DM${deltaM}RP.json --outDir ${OUTPUT}/BDT/DeltaM${deltaM}/VR1 --inDir ${INPUT_SWAP}_bdt${deltaM}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE} --suffix bdt --unblind
 
-    JSONFILE=variablesAN_BDT_VR.json
-    makePlots --json ${JSON_PATH}/plot2016swap_DM${deltaM}RP.json --outDir ${OUTPUT}/BDT/DeltaM${deltaM}/VR1 --inDir ${INPUTS}_bdt${deltaM}/ --variables ${JSONFILE} --cuts ${JSONFILE} --suffix bdt --unblind
+    JSONFILE=variablesAN_BDT_VR2.json
+    makePlots --json ${JSON_PATH}/plot2016_DM${deltaM}RP.json --outDir ${OUTPUT}/BDT/DeltaM${deltaM}/VR2 --inDir ${INPUT}_bdt${deltaM}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE} --suffix bdt --unblind
+
+    JSONFILE=variablesAN_BDT_VR3.json
+    makePlots --json ${JSON_PATH}/plot2016_DM${deltaM}RP.json --outDir ${OUTPUT}/BDT/DeltaM${deltaM}/VR3 --inDir ${INPUT}_bdt${deltaM}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE} --suffix bdt --unblind
   done
 fi
