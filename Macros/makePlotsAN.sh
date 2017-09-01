e3c8469eafd46ebccb413910e5a98ddfa49d4f7a
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-09-01 10:55:50 +0100

    Added the correlation plots to the AN plotting routine

--- Macros/makePlotsAN.sh
+++ Macros/makePlotsAN.sh
@@ -16,24 +16,34 @@ if [[ -d ${INPUT} ]] ; then
     mkdir -p ${OUTPUT}/DataMC/VR1
     mkdir -p ${OUTPUT}/DataMC/VR2
     mkdir -p ${OUTPUT}/DataMC/VR3
+    mkdir -p ${OUTPUT}/Corr/AR
+    mkdir -p ${OUTPUT}/Corr/LNT
+    mkdir -p ${OUTPUT}/Corr/VR1
+    mkdir -p ${OUTPUT}/Corr/VR2
+    mkdir -p ${OUTPUT}/Corr/VR3
   fi
 
   VARIABLE_JSON=variablesAN_DataMC.json
 
   JSONFILE=variablesAN_DataMC.json
   makePlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/DataMC/AR/ --inDir ${INPUT_TEST}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
+  makeTwoDPlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/Corr/AR/ --inDir ${INPUT_TEST}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
 
   JSONFILE=variablesAN_DataMC_LNT.json
   makePlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/DataMC/LNT/ --inDir ${INPUT}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
+  makeTwoDPlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/Corr/LNT/ --inDir ${INPUT}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
 
   JSONFILE=variablesAN_DataMC_VR1.json
   makePlots --json ${JSON_PATH}/plot2016swap_lep.json --outDir ${OUTPUT}/DataMC/VR1/ --inDir ${INPUT_SWAP}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
+  makeTwoDPlots --json ${JSON_PATH}/plot2016swap_lep.json --outDir ${OUTPUT}/Corr/VR1/ --inDir ${INPUT_SWAP}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
 
   JSONFILE=variablesAN_DataMC_VR2.json
   makePlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/DataMC/VR2/ --inDir ${INPUT}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
+  makeTwoDPlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/Corr/VR2/ --inDir ${INPUT}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
 
   JSONFILE=variablesAN_DataMC_VR3.json
   makePlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/DataMC/VR3/ --inDir ${INPUT}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
+  makeTwoDPlots --json ${JSON_PATH}/plot2016_lep.json --outDir ${OUTPUT}/Corr/VR3/ --inDir ${INPUT}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE}
 
   for deltaM in 10 20 30 40 50 60 70 80; do
     mkdir -p ${OUTPUT}/BDT/DeltaM${deltaM}/AR
