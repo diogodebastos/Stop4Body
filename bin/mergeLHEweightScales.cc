a4d6ea29b86556d76850ef856e5bf9adf9e7ad24
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-09-19 15:40:01 +0100

    Added a test merger

---
+++ bin/mergeLHEweightScales.cc
@@ -0,0 +1,100 @@
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <algorithm>
+#include <iomanip>
+#include <math.h>
+#include <memory>
+#include <cmath>
+#include <cstdlib>
+
+#include "TROOT.h"
+#include "TFile.h"
+#include "TDirectory.h"
+#include "TChain.h"
+#include "TObject.h"
+#include "TCanvas.h"
+#include "TMath.h"
+#include "TLegend.h"
+#include "TGraph.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "TTree.h"
+#include "TF1.h"
+#include "TCutG.h"
+#include "TGraphErrors.h"
+#include "TGraphAsymmErrors.h"
+#include "TMultiGraph.h"
+#include "TPaveText.h"
+#include "THStack.h"
+#include "TStyle.h"
+#include "TLatex.h"
+#include "TLorentzVector.h"
+#include "TVectorD.h"
+
+#include "UserCode/Stop4Body/interface/json.hpp"
+#include "UserCode/Stop4Body/interface/SampleReader.h"
+#include "UserCode/Stop4Body/interface/commonFunctions.h"
+#include "UserCode/Stop4Body/interface/doubleWithUncertainty.h"
+
+#define LHEWEIGHT_LIMIT  500
+
+int main(int argc, char** argv)
+{
+  std::string outFile = "";
+  std::vector<std::string> inFiles;
+  inFiles.clear();
+
+  if(argc < 3)
+  {
+    std::cout << "You did not pass enough parameters" << std::endl;
+    //printHelp();
+    return 0;
+  }
+
+  outFile = argv[1];
+
+  for(int i = 2; i < argc; ++i)
+  {
+    std::string argument = argv[i];
+
+    if(fileExists(argument))
+      inFiles.push_back(argument);
+    else
+    {
+      std::cout << "The file " << argument << " does not exist." << std::endl;
+    }
+  }
+
+  if(outFile == "")
+  {
+    std::cout << "You must define an output file" << std::endl;
+    return 1;
+  }
+
+  if(inFiles.size() <= 0)
+  {
+    std::cout << "You must define one or more files to be merged" << std::endl;
+    return 1;
+  }
+
+  std::cout << "Merging intput files into output file:" << std::endl;
+  std::cout << "  Output: " << outFile << std::endl;
+  for(int i = 0; i < inFiles.size(); ++i)
+  {
+    std::cout << "  Input[" << i << "]: " << inFiles[i] << std::endl;
+  }
+
+  return 0;
+
+  gInterpreter->GenerateDictionary("map<int, double>","map");
+  TClass *mapClass = gROOT->FindSTLClass("std::map<int,double>", true);
+  //TFile foutput((outputDirectory + "/lheWeights.root").c_str(), "RECREATE");
+
+  return 0;
+}
