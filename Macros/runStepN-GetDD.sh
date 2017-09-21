2a520cbfa85730e1974cc245faaadfa5326e01ab
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-09-21 16:00:54 +0100

    Added a call for the fake estimate and added the unblinding option

--- Macros/runStepN-GetDD.sh
+++ Macros/runStepN-GetDD.sh
@@ -5,7 +5,8 @@
 
 BASE_OUT_DIR=/lstore/cms/cbeiraod/Stop4Body/DDEstimate
 
-python getAllDDEstimate.py -i ${TEST_DIR} -o ${BASE_OUT_DIR}
+python getAllDDEstimate.py -i ${TEST_DIR} -o ${BASE_OUT_DIR} -u
+python getAllDDEstimate.py -i ${NTUPLE_DIR} -o ${BASE_OUT_DIR}_full -u
 python getAllDDEstimate.py -i ${SWAP_DIR} -o ${BASE_OUT_DIR} --isSwap
 python getAllDDEstimate.py -i ${NTUPLE_DIR} -o ${BASE_OUT_DIR} --VR2
 python getAllDDEstimate.py -i ${NTUPLE_DIR} -o ${BASE_OUT_DIR}_special --VR2 --isSpecial
