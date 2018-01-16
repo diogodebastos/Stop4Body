c1eb437c1f9192360cb8c160e913d5c502df2bce
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2018-01-16 01:22:37 +0000

    Added a quick plotter

---
+++ Macros/Tests/QuickPlotter/DM70_ARjob.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+alias cmsenv='eval `scramv1 runtime -sh`'
+
+cd /exper-sw/cmst3/cmssw/users/cbeiraod/
+. setup.sh
+
+cd /exper-sw/cmst3/cmssw/users/cbeiraod/Stop4Body/CMSSW_8_0_14/src/
+#cd $CMSSW_BASE/src/
+eval `scramv1 runtime -sh`
+
+#cd /exper-sw/cmst3/cmssw/users/cbeiraod/CMSSW_8_0_14/src/UserCode/Stop4Body/Macros/
+cd UserCode/Stop4Body/Macros/
+
+. setupJSONs.sh
+. setupPaths.sh
+
+INPUT=~/local-area/Stop4Body/nTuples_v2017-10-19
+INPUT_TEST=~/local-area/Stop4Body/nTuples_v2017-10-19_test
+INPUT_SWAP=~/local-area/Stop4Body/nTuples_v2017-08-13_swap
+OUTPUT=~/local-area/Stop4Body/ANPlots/
+
+deltaM=70
+
+if [[ -d ${INPUT} ]] ; then
+  VARIABLE_JSON=variablesAN_BDT_AR.json
+
+  JSONFILE=variablesAN_BDT_AR.json
+  makePlots --json ${JSON_PATH}/plot2016_DM${deltaM}RP.json --outDir ${OUTPUT}/BDT/DeltaM${deltaM}/AR --inDir ${INPUT_TEST}_bdt${deltaM}/ --variables ${VARIABLE_JSON} --cuts ${JSONFILE} --suffix bdt --final
+fi
---
+++ Macros/Tests/QuickPlotter/quickPlotter.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+. setupJSONs.sh
+. setupPaths.sh
+
+INPUT=~/local-area/Stop4Body/nTuples_v2017-10-19
+INPUT_TEST=~/local-area/Stop4Body/nTuples_v2017-10-19_test
+INPUT_SWAP=~/local-area/Stop4Body/nTuples_v2017-08-13_swap
+OUTPUT=~/local-area/Stop4Body/ANPlots/
+
+if [[ -d ${INPUT} ]] ; then
+  if [[ ! -d ${OUTPUT} ]] ; then
+    mkdir -p ${OUTPUT}/DataMC/AR
+    mkdir -p ${OUTPUT}/DataMC/LNT
+    mkdir -p ${OUTPUT}/DataMC/VR1
+    mkdir -p ${OUTPUT}/DataMC/VR2
+    mkdir -p ${OUTPUT}/DataMC/VR3
+    mkdir -p ${OUTPUT}/Corr/AR
+    mkdir -p ${OUTPUT}/Corr/LNT
+    mkdir -p ${OUTPUT}/Corr/VR1
+    mkdir -p ${OUTPUT}/Corr/VR2
+    mkdir -p ${OUTPUT}/Corr/VR3
+  fi
+
+  for deltaM in 10 20 30 40 50 60 70 80; do
+    mkdir -p ${OUTPUT}/BDT/DeltaM${deltaM}/AR
+    mkdir -p ${OUTPUT}/BDT/DeltaM${deltaM}/LNT
+    mkdir -p ${OUTPUT}/BDT/DeltaM${deltaM}/VR1
+    mkdir -p ${OUTPUT}/BDT/DeltaM${deltaM}/VR2
+    mkdir -p ${OUTPUT}/BDT/DeltaM${deltaM}/VR3
+  done
+
+
+  ### Submit the jobs
+  qsub ARjob.sh
+  #qsub LNTjob.sh
+  #qsub VR1job.sh
+  qsub VR2job.sh
+  qsub VR3job.sh
+
+  for deltaM in 10 20 30 40 50 60 70 80; do
+    qsub DM${deltaM}_ARjob.sh
+    #qsub DM${deltaM}_LNTjob.sh
+    #qsub DM${deltaM}_VR1job.sh
+    qsub DM${deltaM}_VR2job.sh
+    qsub DM${deltaM}_VR3job.sh
+  done
+fi
