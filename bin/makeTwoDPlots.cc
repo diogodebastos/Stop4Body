e54104f86587b85d65689758838b20dc4a7519f6
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2018-02-28 21:14:04 +0000

    More messing around with margins

--- bin/makeTwoDPlots.cc
+++ bin/makeTwoDPlots.cc
@@ -335,11 +335,11 @@ int main(int argc, char** argv)
 
           c1.SetRightMargin(0.0);
           c1.SetLeftMargin(0.0);
+          c1.SetTopMargin(0.0);
 
           TVirtualPad* thisPad = c1.cd(1);
-          tdrstyle->cd();
           thisPad->SetLogz(true);
-          //thisPad->SetTopMargin(0.05);
+          thisPad->SetTopMargin(0.05);
           //thisPad->SetBottomMargin(0.10);
           thisPad->SetRightMargin(0.16);
           thisPad->SetLeftMargin(0.16);
@@ -373,9 +373,8 @@ int main(int argc, char** argv)
           drawInfo(thisPad, luminosity, !final);
 
           thisPad = c1.cd(2);
-          tdrstyle->cd();
           thisPad->SetLogz(true);
-          //thisPad->SetTopMargin(0.05);
+          thisPad->SetTopMargin(0.05);
           //thisPad->SetBottomMargin(0.10);
           thisPad->SetRightMargin(0.16);
           thisPad->SetLeftMargin(0.16);
@@ -411,9 +410,8 @@ int main(int argc, char** argv)
           if(plotData)
           {
             thisPad = c1.cd(3);
-            tdrstyle->cd();
             thisPad->SetLogz(true);
-            //thisPad->SetTopMargin(0.05);
+            thisPad->SetTopMargin(0.05);
             //thisPad->SetBottomMargin(0.10);
             thisPad->SetRightMargin(0.16);
             thisPad->SetLeftMargin(0.16);
