7a6cf3878171d25be914be28a94f988762e96bde
diogodebastos <diogodebastos18@gmail.com>
2018-04-04 14:20:40 +0100

    parse only filename

--- Macros/plotStep0.py
+++ Macros/plotStep0.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     parser.add_argument('-f', '--file1', required=True, help='File1 name')
     parser.add_argument('-k', '--file2', required=True, help='File2 name')
     parser.add_argument('-p', '--histogram', required=True, help='Histogram name to be plotted')
+    parser.add_argument('-u', '--unified', help='Give only the name of the file and the hist')
 
     args = parser.parse_args()
     file1Name=args.file1
@@ -20,9 +21,16 @@ if __name__ == "__main__":
 #    ROOT.gStyle.SetOptTitle(0)
 
     c = TCanvas("c",histName,800,600)
-    
-    f1 = ROOT.TFile(file1Name,"READ")
-    f2 = ROOT.TFile(file2Name,"READ")
+
+    if args.unified:
+        cbeiraodPath="/lstore/cms/cbeiraod/Stop4Body/puWeights"
+        dbastosPath="/lstore/cms/dbastos/Stop4Body/nTuples_v2018-04-03/"
+
+        f1 = ROOT.TFile(cbeiraodPath+file1Name,"READ")
+        f2 = ROOT.TFile(dbastosPath+file1Name,"READ")
+    else:
+        f1 = ROOT.TFile(file1Name,"READ")
+        f2 = ROOT.TFile(file2Name,"READ")
 
     h1 = f1.Get(histName)
     h2 = f2.Get(histName)
@@ -32,16 +40,16 @@ if __name__ == "__main__":
 
     h1.Draw()
     h2.Draw("same")
-    
+
     h1.SetTitle("H1")
     h2.SetTitle("H2")
-    
+
     c.BuildLegend(0.9,0.8,1,0.9)
     h1.SetTitle(histName)
     #c.cd()
     #Tl = ROOT.TLatex(0.5,0.9,histName)
     #Tl.SetTextSize(0.04)
-    #Tl.SetTextAlign(13)    
+    #Tl.SetTextAlign(13)
     #Tl.Draw()
 
 
