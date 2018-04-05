1656769f5d9295ab3251a0cc60a1d9f0390ea9d5
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2018-04-05 14:26:36 +0100

    Fixed Step1 for the change in Step0

--- Macros/runStep1.sh
+++ Macros/runStep1.sh
@@ -5,6 +5,15 @@
 
 if [[ ! -d ${NTUPLE_DIR} ]] ; then
   mkdir -p ${NTUPLE_DIR}
+  if [[ ! -d ${PUWEIGHT_DIR} ]] ; then
+    echo "Please run Step0 first"
+    exit 1
+  fi
+  if [[ ! -f ${PUWEIGHT_DIR}/puWeights.root ]] ; then
+    echo "Please run Step0 first"
+    exit 1
+  fi
+  cp ${PUWEIGHT_DIR}/puWeights.root ${NTUPLE_DIR}
 fi
 if [[ ! -d ${TEST_DIR} ]] ; then
   mkdir -p ${TEST_DIR}
