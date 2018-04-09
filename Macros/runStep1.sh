4dddbd933105f0b860b83e3a7260b670bb6c3c62
diogodebastos <diogodebastos18@gmail.com>
2018-04-09 19:36:31 +0100

    Add check for LHEWeights

--- Macros/runStep1.sh
+++ Macros/runStep1.sh
@@ -14,6 +14,15 @@ if [[ ! -d ${NTUPLE_DIR} ]] ; then
     exit 1
   fi
   cp ${PUWEIGHT_DIR}/puWeights.root ${NTUPLE_DIR}
+  if [[ ! -d ${LHESCALE_DIR} ]] ; then
+    echo "Please run Step0-LHEWeight first"
+    exit 1
+  fi
+  if [[ ! -f ${LHESCALE_DIR}/lheWeights.root ]] ; then
+    echo "Please run Step0-LHEWheigt_part2 first"
+    exit 1
+  fi
+  cp ${LHESCALE_DIR}/lheWeights.root ${NTUPLE_DIR}
 fi
 if [[ ! -d ${TEST_DIR} ]] ; then
   mkdir -p ${TEST_DIR}
