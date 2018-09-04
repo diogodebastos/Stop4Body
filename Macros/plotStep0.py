658bd97b12ac88e6ab7aa76518a7a7cd1f02319f
diogodebastos <diogodebastos18@gmail.com>
2018-09-04 18:05:49 +0100

    minor fix

--- Macros/plotStep0.py
+++ Macros/plotStep0.py
@@ -50,8 +50,8 @@ if __name__ == "__main__":
     h2.Draw("same")
 
     h1.SetTitle(file1Name)
-    h1.GetXaxis().SetTitle("X axis title");
-    h1.GetYaxis()->SetTitle("Y axis title");
+    h1.GetXaxis().SetTitle("X axis title")
+    h1.GetYaxis()->SetTitle("Y axis title")
     h2.SetTitle(file2Name")
     
     c.BuildLegend(0.9,0.8,1,0.9)
