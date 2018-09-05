ca5650846f02eaa565014b7047f7cd003f4e6832
Diogo de Bastos <diogodebastos18@gmail.com>
2018-09-05 16:26:33 +0100

    plot Tree in python

--- Macros/plotStep0.py
+++ Macros/plotStep0.py
@@ -1,5 +1,5 @@
 import ROOT
-from ROOT import TFile, TH1F, TCanvas
+from ROOT import TFile, TH1F, TCanvas, TTree
 
 if __name__ == "__main__":
     import sys
@@ -10,17 +10,17 @@ if __name__ == "__main__":
     parser.add_argument('-f', '--file1', required=True, help='File1 name')
     parser.add_argument('-k', '--file2', help='File2 name')
     parser.add_argument('-p', '--histogram', help='Histogram name to be plotted')
-    parser.add_argument('-u', '--unified', action='store_true', help='Give only the name of the file and the hist')
+    parser.add_argument('-y', '--year', action='store_true', help='2016 vs 2017')
     parser.add_argument(      '--pileUp', action='store_true',help='plot pileUp distribution')
 
     args = parser.parse_args()
     file1Name=args.file1
     file2Name=args.file2
     ROOT.gStyle.SetOptStat("00000000")
-#    ROOT.gStyle.SetOptTitle(0)
-
+#   ROOT.gStyle.SetOptTitle(0)
+#   ROOT.gStyle.SetPadTickY(1)
     
-    if args.unified:
+    if args.year:
         cbeiraodPath="/lstore/cms/cbeiraod/Stop4Body/puWeights/"
         #dbastosPath="/lstore/cms/dbastos/Stop4Body/nTuples_v2018-04-09/"
         dbastosPath="/lstore/cms/dbastos/Stop4Body/puWeights/2017/"      
@@ -28,33 +28,36 @@ if __name__ == "__main__":
         f2 = ROOT.TFile(dbastosPath+file1Name,"READ")
         file2Name=file1Name
         histName=args.histogram
+        h1 = f1.Get(histName)
+        h2 = f2.Get(histName)
     elif args.pileUp:
         basePath = "/lstore/cms/dbastos/Stop4Body/"
         nTupleDir = "nTuples_v2018-08-24/"
         f1 = ROOT.TFile(basePath+nTupleDir+file1Name,"READ")
         f2 = ROOT.TFile(basePath+nTupleDir+file2Name,"READ")
+        bdttree.Print()
         histName = "nVert"
+        h1 = f1.getHist(histName)
+        h2 = f2.getHist(histName)
     else:
         f1 = ROOT.TFile(file1Name,"READ")
         f2 = ROOT.TFile(file2Name,"READ")
 
-    c = TCanvas("c",histName,1200,800)
-    
-    h1 = f1.Get(histName)
-    h2 = f2.Get(histName)
+    c1 = TCanvas("c1",histName, 1200, 1350)
+    #c1.Range()
 
     h1.SetLineColor(ROOT.kBlue)
     h2.SetLineColor(ROOT.kRed)
 
     h1.Draw()
     h2.Draw("same")
-
+    
     h1.SetTitle(file1Name)
     h1.GetXaxis().SetTitle("X axis title")
     h1.GetYaxis().SetTitle("Y axis title")
     h2.SetTitle(file2Name)
     
-    c.BuildLegend(0.9,0.8,1,0.9)
+    c1.BuildLegend(0.9,0.8,1,0.9)
     h1.SetTitle(histName)
     #c.cd()
     #Tl = ROOT.TLatex(0.5,0.9,histName)
@@ -70,5 +73,5 @@ if __name__ == "__main__":
 #/lstore/cms/dbastos/Stop4Body/nTuples_v2018-04-03/
 #puWeights_stop675.json.root
 #python plotStep0.py -f /lstore/cms/dbastos/Stop4Body/nTuples_v2018-04-03/puWeights_stop675.json.root -k /lstore/cms/cbeiraod/Stop4Body/puWeights/puWeights_stop675.json.root -p sample_T2DegStop_675_595_nTrueInt
-#python plotStep0.py -u -f puWeights_Wjets.json.root -p process_WJets_puWeight
+#python plotStep0.py -y -f puWeights_Wjets.json.root -p process_WJets_puWeight
 #python plotStep0.py --pileUp -f Data_2017C_MetHT.root -k TTJets.root
---
+++ Macros/plotTree.py
@@ -0,0 +1,39 @@
+import ROOT
+from ROOT import TFile, TH1F, TCanvas, TTree, THStack
+
+path="/lstore/cms/dbastos/Stop4Body/nTuples_v2018-08-24/"
+nTuple1="ZJetsToNuNu_HT600to800.root"
+nTuple2="Data_2017B_MetHT.root"
+
+ROOT.gStyle.SetOptStat("00000000")
+
+f1=ROOT.TFile(path+nTuple1,"READ")
+f2=ROOT.TFile(path+nTuple2,"READ")
+
+t1=f1.Get("bdttree")
+t2=f2.Get("bdttree")
+
+h1 = TH1F("h1","nVert",150,0,150)
+h2 = TH1F("h2","nVert",150,0,150)
+h1.SetLineColor(ROOT.kBlue)
+h2.SetLineColor(ROOT.kRed)
+
+t1.Draw("nVert>>h1","","goff")
+t2.Draw("nVert>>h2","","goff")
+
+b1=t1.GetBranch("nVert")
+b2=t2.GetBranch("nVert")
+
+nE1=b1.GetEntries()
+
+hs= THStack("hs","")
+hs.Add(h1)
+hs.Add(h2)
+
+hs.Draw()
+
+#for i in range(nE1):
+#  h1.Fill(b1.GetEntry(i))
+
+#c1 = TCanvas("c1","name",1200,800)
+
