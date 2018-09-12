273566a3a0bb81f5caf03e8953b80d3f60e9ab38
diogodebastos <diogodebastos18@gmail.com>
2018-09-12 17:48:59 +0100

    edit previous commit

--- Macros/plotTree.py
+++ Macros/plotTree.py
@@ -6,48 +6,58 @@ nVert="/home/t3cms/dbastos/LSTORE/Stop4Body/nTuples_v2018-09-06/DataMC_PU/Presel
 ROOT.gStyle.SetOptStat("00000000")
 
 fNVert = ROOT.TFile(nVert,"READ")
+hs= THStack("hs","")
+c1 = TCanvas("c1","nVert", 1200, 1350)
 
 data = fNVert.Get("Data")
 data.Scale(1/data.Integral())
 data.SetLineColor(ROOT.kBlue)
-
-ttbar = fNVert.Get("ttbar")
-ttbar.Scale(1/ttbar.Integral())
-ttbar.SetLineColor(ROOT.kRed)
-
-ttbar = fNVert.Get("ttbar")
-ttbar.Scale(1/ttbar.Integral())
-ttbar.SetLineColor(ROOT.kRed)
-
-ttbar = fNVert.Get("ttbar")
-ttbar.Scale(1/ttbar.Integral())
-ttbar.SetLineColor(ROOT.kRed)
-
-ttbar = fNVert.Get("ttbar")
-ttbar.Scale(1/ttbar.Integral())
-ttbar.SetLineColor(ROOT.kRed)
-
-ttbar = fNVert.Get("ttbar")
-ttbar.Scale(1/ttbar.Integral())
-ttbar.SetLineColor(ROOT.kRed)
-
-ttbar = fNVert.Get("ttbar")
-ttbar.Scale(1/ttbar.Integral())
-ttbar.SetLineColor(ROOT.kRed)
-
-ttbar = fNVert.Get("ttbar")
-ttbar.Scale(1/ttbar.Integral())
-ttbar.SetLineColor(ROOT.kRed)
-
-hs= THStack("hs","")
 hs.Add(data)
+
+ttbar = fNVert.Get("ttbar")
+ttbar.Scale(1/ttbar.Integral())
+ttbar.SetLineColor(ROOT.kRed)
 hs.Add(ttbar)
 
-c1 = TCanvas("c1","nVert", 1200, 1350)
+WJets = fNVert.Get("WJets")
+WJets.Scale(1/WJets.Integral())
+WJets.SetLineColor(ROOT.kRed)
+hs.Add(WJets)
+
+ZInv = fNVert.Get("ZInv")
+ZInv.Scale(1/ZInv.Integral())
+ZInv.SetLineColor(ROOT.kRed)
+hs.Add(ZInv)
+
+VV = fNVert.Get("VV")
+VV.Scale(1/VV.Integral())
+VV.SetLineColor(ROOT.kRed)
+hs.Add(VV)
+
+DYJets = fNVert.Get("DYJets")
+DYJets.Scale(1/DYJets.Integral())
+DYJets.SetLineColor(ROOT.kRed)
+hs.Add(DYJets)
+
+SingleTop = fNVert.Get("SingleTop")
+SingleTop.Scale(1/SingleTop.Integral())
+SingleTop.SetLineColor(ROOT.kRed)
+hs.Add(SingleTop)
+
+QCD = fNVert.Get("QCD")
+QCD.Scale(1/QCD.Integral())
+QCD.SetLineColor(ROOT.kRed)
+hs.Add(QCD)
+
+ttx = fNVert.Get("ttx")
+ttx.Scale(1/ttx.Integral())
+ttx.SetLineColor(ROOT.kRed)
+hs.Add(ttx)
+
 hs.Draw()
 c1.BuildLegend(0.66, 0.85, 0.87, 0.75, "")
 
-
+########
 
 path="/lstore/cms/dbastos/Stop4Body/nTuples_v2018-08-24/"
 nTuple1="ZJetsToNuNu_HT600to800.root"
