c29057a6af71a6bc67bb767211472fc771b2d2da
Diogo de Bastos <diogodebastos18@gmail.com>
2018-09-13 18:48:21 +0100

    plot MC vs Data variables for first test

--- Macros/StepN_makeDataMC.sh
+++ Macros/StepN_makeDataMC.sh
@@ -7,7 +7,7 @@ INPUT=${NTUPLE_DIR}
 OUTPUT=${INPUT}/DataMC
 OUTPUT_POW=${INPUT}/DataMC_pow
 OUTPUT_LEP=${INPUT}/DataMC_lep
-
+OUTPUT_TEST=${INPUT}/DataMC_test
 
 if [[ -d ${INPUT} ]] ; then
   if [[ ! -d ${OUTPUT} ]] ; then
@@ -19,7 +19,11 @@ if [[ -d ${INPUT} ]] ; then
   if [[ ! -d ${OUTPUT_LEP} ]] ; then
     mkdir -p ${OUTPUT_LEP}
   fi
-  makePlots --json ${JSON_PATH}/plot${YEAR}.json     --outDir ${OUTPUT}     --inDir ${INPUT} --variables variables.json --cuts variables.json
+  if [[ ! -d ${OUTPUT_TEST} ]] ; then
+    mkdir -p ${OUTPUT_TEST}
+  fi
+  #makePlots --json ${JSON_PATH}/plot${YEAR}.json     --outDir ${OUTPUT}     --inDir ${INPUT} --variables variables.json --cuts variables.json
   #makePlots --json ${JSON_PATH}/plot${YEAR}_pow.json --outDir ${OUTPUT_POW} --inDir ${INPUT} --variables variables.json --cuts variables.json
   #makePlots --json ${JSON_PATH}/plot${YEAR}_lep.json --outDir ${OUTPUT_LEP} --inDir ${INPUT} --variables variables.json --cuts variables.json
+  makePlots --json ${JSON_PATH}/plot${YEAR}.json --outDir ${OUTPUT_TEST} --inDir ${INPUT} --variables variablesTest.json --puTest
 fi
