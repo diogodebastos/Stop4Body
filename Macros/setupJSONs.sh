5555ed903acf57a99808b8d08437d840aaccfdfc
diogodebastos <diogodebastos18@gmail.com>
2018-09-19 19:38:19 +0100

    change order of stacking; plot FS: singleEl, singleMu and both

--- Macros/setupJSONs.sh
+++ Macros/setupJSONs.sh
@@ -114,14 +114,13 @@ elif [ ${YEAR} == 2017 ]; then
  # Make a JSON for all MC except signal
  mergeJSONs $JSON_PATH/Orig/Wjets.json               $JSON_PATH/Orig/W_*Jets*.json
  mergeJSONs $JSON_PATH/Orig/ZInv.json                $JSON_PATH/Orig/ZJetsToNuNu*.json
- # mergeJSONs $JSON_PATH/backgroundMC.json       $JSON_PATH/Orig/TTbar.json $JSON_PATH/Orig/Wjets.json
  cp         $JSON_PATH/Orig/TTbar.json               $JSON_PATH/TTbar.json
  cp         $JSON_PATH/Orig/Wjets.json               $JSON_PATH/Wjets.json
  cp         $JSON_PATH/Orig/ZInv.json                $JSON_PATH/ZInv.json
+ mergeJSONs $JSON_PATH/ttX.json                      $JSON_PATH/Orig/TTGJets.json $JSON_PATH/Orig/TTW_LO.json $JSON_PATH/Orig/TTWToLNu.json $JSON_PATH/Orig/TTZToLLNuNu.json
  mergeJSONs $JSON_PATH/otherMC1.json                 $JSON_PATH/Orig/VV.json $JSON_PATH/Orig/DYJets.json
  mergeJSONs $JSON_PATH/otherMC2.json                 $JSON_PATH/Orig/SingleTop.json $JSON_PATH/Orig/QCD.json $JSON_PATH/Orig/TTGJets.json $JSON_PATH/Orig/TTW_LO.json $JSON_PATH/Orig/TTWToLNu.json $JSON_PATH/Orig/TTZToLLNuNu.json
- mergeJSONs $JSON_PATH/allMC.json                    $JSON_PATH/Wjets.json $JSON_PATH/TTbar.json $JSON_PATH/Orig/SingleTop.json $JSON_PATH/Orig/VV.json $JSON_PATH/ZInv.json $JSON_PATH/Orig/DYJets.json $JSON_PATH/Orig/QCD.json $JSON_PATH/Orig/TTGJets.json $JSON_PATH/Orig/TTW_LO.json $JSON_PATH/Orig/TTWToLNu.json $JSON_PATH/Orig/TTZToLLNuNu.json
-
+ mergeJSONs $JSON_PATH/allMC.json                    $JSON_PATH/Orig/QCD.json $JSON_PATH/ttX.json $JSON_PATH/ZInv.json $JSON_PATH/Orig/VV.json $JSON_PATH/Orig/SingleTop.json $JSON_PATH/Orig/DYJets.json $JSON_PATH/TTbar.json $JSON_PATH/Wjets.json
  #Make a JSON with the samples that are to be processed:
  cp         $JSON_PATH/allMC.json                    $JSON_PATH/MC2Process.json
  
