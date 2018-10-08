389aa15dceee8f41d25382af330ec83a5ff79cec
diogodebastos <diogodebastos18@gmail.com>
2018-10-08 19:17:49 +0100

    DATA/MC plot all 12 vars

--- Macros/StepN_makeDataMC.sh
+++ Macros/StepN_makeDataMC.sh
@@ -7,7 +7,7 @@ INPUT=${NTUPLE_DIR}
 OUTPUT=${INPUT}/DataMC
 OUTPUT_POW=${INPUT}/DataMC_pow
 OUTPUT_LEP=${INPUT}/DataMC_lep
-OUTPUT_TEST=${INPUT}/DataMC_test
+OUTPUT_2017=${INPUT}/DataMC_2017
 
 if [[ -d ${INPUT} ]] ; then
   if [[ ! -d ${OUTPUT} ]] ; then
@@ -25,5 +25,5 @@ if [[ -d ${INPUT} ]] ; then
   #makePlots --json ${JSON_PATH}/plot${YEAR}.json     --outDir ${OUTPUT}     --inDir ${INPUT} --variables variables.json --cuts variables.json
   #makePlots --json ${JSON_PATH}/plot${YEAR}_pow.json --outDir ${OUTPUT_POW} --inDir ${INPUT} --variables variables.json --cuts variables.json
   #makePlots --json ${JSON_PATH}/plot${YEAR}_lep.json --outDir ${OUTPUT_LEP} --inDir ${INPUT} --variables variables.json --cuts variables.json
-  makePlots --json ${JSON_PATH}/plot${YEAR}.json --outDir ${OUTPUT_TEST} --inDir ${INPUT} --variables variablesLepPt_Nvert.json --puTest
+  makePlots --json ${JSON_PATH}/plot${YEAR}.json     --outDir ${OUTPUT_2017} --inDir ${INPUT} --variables variables2017.json --cuts variables2017.json --puTest
 fi
