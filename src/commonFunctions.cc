c6759b882ff955913d10bb89af6a462253e8c1ba
diogodebastos <diogodebastos18@gmail.com>
2018-11-30 17:06:30 +0000

    initialize sys for lepton ISO

--- src/commonFunctions.cc
+++ src/commonFunctions.cc
@@ -823,19 +823,29 @@ ValueWithSystematics<double> getLeptonISOSF2017Sys(double LepID, double LepPt, d
   double unc = lepISOSF.uncertainty();
 
   ValueWithSystematics<double> retVal(val);
+  retVal.Systematic("LeptonISOSF2017_Electron_stat_Up");
+  retVal.Systematic("LeptonISOSF2017_Electron_stat_Down");
+  retVal.Systematic("LeptonISOSF2017_Electron_Up");
+  retVal.Systematic("LeptonISOSF2017_Electron_Down");
+  
+  retVal.Systematic("LeptonISOSF2017_Muon_stat_Up");
+  retVal.Systematic("LeptonISOSF2017_Muon_stat_Down");
+  retVal.Systematic("LeptonISOSF2017_Muon_Up");
+  retVal.Systematic("LeptonISOSF2017_Muon_Down");
+  retVal.Lock();
   if(std::abs(LepID) == 11)
   {
-   retVal.Systematic("LeptonISOF2017_Electron_stat_Up") = val+unc;
-   retVal.Systematic("LeptonISOF2017_Electron_stat_Down") = val-unc;
-   retVal.Systematic("LeptonISOF2017_Electron_Up") = val*1.01;
-   retVal.Systematic("LeptonISOF2017_Electron_Down") = val*0.99; 
+   retVal.Systematic("LeptonISOSF2017_Electron_stat_Up") = val+unc;
+   retVal.Systematic("LeptonISOSF2017_Electron_stat_Down") = val-unc;
+   retVal.Systematic("LeptonISOSF2017_Electron_Up") = val*1.01;
+   retVal.Systematic("LeptonISOSF2017_Electron_Down") = val*0.99; 
   }
   else if(std::abs(LepID) == 13)
   {
-   retVal.Systematic("LeptonISOF2017_Muon_stat_Up") = val+unc;
-   retVal.Systematic("LeptonISOF2017_Muon_stat_Down") = val-unc;
-   retVal.Systematic("LeptonISOF2017_Muon_Up") = val*1.01;
-   retVal.Systematic("LeptonISOF2017_Muon_Down") = val*0.99;
+   retVal.Systematic("LeptonISOSF2017_Muon_stat_Up") = val+unc;
+   retVal.Systematic("LeptonISOSF2017_Muon_stat_Down") = val-unc;
+   retVal.Systematic("LeptonISOSF2017_Muon_Up") = val*1.01;
+   retVal.Systematic("LeptonISOSF2017_Muon_Down") = val*0.99;
   }
   return retVal;
 }
