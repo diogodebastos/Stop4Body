1ea7e992e00f2d000c84b8d213ff193476c4bcfc
diogodebastos <diogodebastos18@gmail.com>
2019-01-18 15:38:57 +0000

    added cutinfo class to getfakerate

--- bin/getFakeRateEstimate.cc
+++ bin/getFakeRateEstimate.cc
@@ -22,6 +22,22 @@
 
 using json = nlohmann::json;
 
+class CutInfo
+{
+public:
+  CutInfo(std::string name, std::string cut, std::string latex): name_(name), cut_(cut), latex_(latex) {}
+
+  std::string name()  const {return name_;}
+  std::string cut()   const {return cut_;}
+  std::string latex() const {return latex_;}
+
+private:
+protected:
+  std::string name_;
+  std::string cut_;
+  std::string latex_;
+};
+
 int main(int argc, char** argv)
 {
   std::string debug;
