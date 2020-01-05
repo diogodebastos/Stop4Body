779327938ed70f87b91f855dfa153c5485b92551
Diogo de Bastos <diogodebastos18@gmail.com>
2020-01-05 18:00:07 +0000

    QCD_Mu_en PU and LHE

--- Macros/setupJSONs.sh
+++ Macros/setupJSONs.sh
@@ -129,6 +129,8 @@ elif [ ${YEAR} == 2017 ]; then
  mergeJSONs $JSON_PATH/allMC.json                    $JSON_PATH/Orig/QCD.json $JSON_PATH/ttX.json $JSON_PATH/ZInv.json $JSON_PATH/Orig/VV.json $JSON_PATH/Orig/SingleTop.json $JSON_PATH/Orig/DYJets.json $JSON_PATH/TTbar.json $JSON_PATH/Wjets.json
  mergeJSONs $JSON_PATH/allMC-QCD-en.json             $JSON_PATH/QCD_en.json $JSON_PATH/ttX.json $JSON_PATH/ZInv.json $JSON_PATH/Orig/VV.json $JSON_PATH/Orig/SingleTop.json $JSON_PATH/Orig/DYJets.json $JSON_PATH/TTbar.json $JSON_PATH/Wjets.json
 
+ mergeJSONs $JSON_PATH/otherMC2_QCD_Mu_en.json                 $JSON_PATH/Orig/SingleTop.json $JSON_PATH/Orig/QCD_Mu.json $JSON_PATH/Orig/TTGJets.json $JSON_PATH/Orig/TTW_LO.json $JSON_PATH/Orig/TTWToLNu.json $JSON_PATH/Orig/TTZToLLNuNu.json
+
  #Make a JSON with the samples that are to be processed:
  cp         $JSON_PATH/allMC.json                    $JSON_PATH/MC2Process.json
 
