9d23c016ce55e5430d94639e38e84292c4202e31
Diogo de Bastos <diogodebastos18@gmail.com>
2020-03-19 22:40:09 +0000

    this coding is so bad that i am ashamed of myself

--- bin/getJSONlumis.cc
+++ bin/getJSONlumis.cc
@@ -16,12 +16,14 @@
 #include "string"
 #include "vector"
 
-void eraseFileLine(std::string, std::string);
+void fixFile(std::string);
+bool cleanFile(std::string);
 
 int main(int argc, char** argv){
 
   std::string inputDirectory = "";
   std::string outputDirectory = "./OUT/";
+  bool debug = false;
 
   for(int i = 1; i < argc; ++i)
   {
@@ -32,6 +34,9 @@ int main(int argc, char** argv){
 
     if(argument == "--outDir")
       outputDirectory = argv[++i];
+ 
+    if(argument == "--debug")
+      debug = true;
   }
 
   TFile file((inputDirectory+"/tree.root").c_str(),"");
@@ -44,20 +49,22 @@ int main(int argc, char** argv){
   UInt_t luminosityBlock;
   lumiTree->SetBranchAddress("run", &run);
   lumiTree->SetBranchAddress("luminosityBlock", &luminosityBlock);
-/*
-  std::cout << "For DEBUG" << std::endl;
   UInt_t debugBegin = 0;
-  UInt_t debugLength = nentries;//nentries;
-  for(size_t i = debugBegin; i < debugBegin+debugLength; i++)
-  {
-    lumiTree->GetEntry(i);
-    std::cout << " Entry: " << i << "  " << run << " : " << luminosityBlock << std::endl;
+  UInt_t debugEnd = 1000;
+
+  if (debug) {
+    std::cout << "For DEBUG" << std::endl;
+    for(size_t i = debugBegin; i < debugEnd; i++)
+    {
+      lumiTree->GetEntry(i);
+      std::cout << " Entry: " << i << "  " << run << " : " << luminosityBlock << std::endl;
+    }
+    std::cout << std::endl;
   }
-  std::cout << std::endl;
-*/
   lumiTree->GetEntry(0);
 
   UInt_t lastRun = run;
+  UInt_t lastLastRun = run;
   UInt_t lastLumiBlock = luminosityBlock-1;
   UInt_t lastLastLumiBlock = luminosityBlock-2;
   UInt_t firstLumi = 1;
@@ -66,7 +73,7 @@ int main(int argc, char** argv){
   jsonFile.open ((outputDirectory+"/lumiSummary.json").c_str());
   jsonFile << "{" << '"' << lastRun << '"' << ": [[" << luminosityBlock << ",";
 
-  for(size_t i = 0; i < nentries; i++)
+  for(size_t i = debugBegin; i < debugEnd; i++)
   {
     lumiTree->GetEntry(i);
     if (lastRun==run) {
@@ -83,7 +90,7 @@ int main(int argc, char** argv){
           }
           ++firstLumi;
         }
-        else if ((luminosityBlock-lastLumiBlock)!=1) {
+        else if ((luminosityBlock-lastLumiBlock)!=1 && lastLastRun == lastRun) {
           jsonFile << lastLumiBlock << "]";
         }
       }
@@ -95,9 +102,13 @@ int main(int argc, char** argv){
       else if ((luminosityBlock-lastLumiBlock)!=1) {
         jsonFile << "],";
       }
+      else if ((luminosityBlock-lastLumiBlock)==1 && (lastLumiBlock-lastLastLumiBlock)!=1) {
+        jsonFile << "],";
+      }
       jsonFile << "\n " << '"' << run << '"' << ": [";
       firstLumi=0;
     }
+    lastLastRun=lastRun;
     lastRun=run;
     lastLastLumiBlock=lastLumiBlock;
     lastLumiBlock=luminosityBlock;
@@ -107,15 +118,18 @@ int main(int argc, char** argv){
   }
   else{
     //jsonFile << "[" << lastLumiBlock << "]]}";
+    jsonFile << "]}";
   }
-  jsonFile << std::endl;
   jsonFile.close();
-  eraseFileLine(outputDirectory,": [],");
+  bool removeLastLine = cleanFile(outputDirectory);
+  
+  if (removeLastLine){
+    fixFile(outputDirectory);
+  }
+
 }
 
-
-//https://stackoverflow.com/questions/26576714/deleting-specific-line-from-file
-void eraseFileLine(std::string path, std::string eraseLine) {
+void fixFile(std::string path) {
   std::string line;
   std::ifstream fin;
 
@@ -123,17 +137,67 @@ void eraseFileLine(std::string path, std::string eraseLine) {
   std::ofstream temp;
   temp.open(path+"/temp.json");
 
+  UInt_t nLines=0;
+  UInt_t counter=0;
+
   while (getline(fin, line)) {
-      if (line.substr( line.length()-5) != eraseLine){
+    ++nLines;  
+  }
+  fin.close();
+  fin.open(path+"/lumiSummary.json");
+
+  while (getline(fin, line)) {
+    ++counter;
+    if (counter == nLines - 1) {
+      temp << line.substr(0, line.length()-2) << "}" << std::endl; 
+    }
+    else if (counter == nLines){
+      temp << "";
+    }
+    else {
+      temp << line << std::endl;
+    }
+  }
+ 
+  temp.close();
+  fin.close();
+
+  std::string phStr = path+"/lumiSummary.json";
+  const char * p = phStr.c_str();
+  remove(p);
+  rename((path+"/temp.json").c_str(), p);
+}
+
+//https://stackoverflow.com/questions/26576714/deleting-specific-line-from-file
+bool cleanFile(std::string path) {
+  std::string line;
+  std::ifstream fin;
+
+  fin.open(path+"/lumiSummary.json");
+  std::ofstream temp;
+  temp.open(path+"/temp.json");
+
+  bool fixLastLine = false;
+  while (getline(fin, line)) {
+    if (line.length() > 5 ){
+      if ((line.substr( line.length()-5) == ": [],") || (line.substr( line.length()-4) == ": []") || (line.substr( line.length()-3) == ": [") ){
+        temp << "";
+      }
+      else{
+        if(line.substr(line.length()-5) == ": []}"){
+          fixLastLine = true;
+        }
         temp << line << std::endl;
       }
+    }
   }
 
   temp.close();
   fin.close();
 
   std::string phStr = path+"/lumiSummary.json";
-  const char * p = phStr.c_str(); // required conversion for remove and rename functions
+  const char * p = phStr.c_str();
   remove(p);
   rename((path+"/temp.json").c_str(), p);
+  return fixLastLine;
 }
