8c0b0473e484db15c2bbfd7539149e68d7736595
Diogo de Bastos <diogodebastos18@gmail.com>
2021-05-01 19:26:40 +0100

    update 2016 datacards for uncorrelates sys and lumi scheme

---
+++ Macros/update2016dataCards.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import os, sys
+
+YEAR = os.getenv("YEAR")
+#pathInp = "/Users/ketchum/tmp/DataCards/2016"
+#pathOut = "/Users/ketchum/tmp/DataCards/2016_new"
+pathInp = "DataCards/2016"
+pathOut = "DataCards/2016_new"
+dirs = os.listdir(pathInp)
+
+def updateLine(line, sys, out):
+  updated = False 
+  if line.startswith(sys):
+    if sys == "Lum":
+      line = "Lum16     lnN 1.01 - - - 1.01 1.01 1.01 1.01"
+    else:
+      line = line.replace(sys, sys+"16")
+    out.write(line)
+    if sys == "Lum": out.write("\nLum161718 lnN 1.006 - - - 1.006 1.006 1.006 1.006\n")
+    updated = True
+  return updated
+
+for file in dirs:
+  with open(pathInp+"/"+file, 'r') as inp, open(pathOut+"/"+file, 'w') as out:
+    for line in inp:
+      if line.strip():
+
+        Sstline   = updateLine(line,"Sst",out)
+        FastSline = updateLine(line,"FastS",out)
+        VVstline  = updateLine(line,"VVst",out)
+        STstline  = updateLine(line,"STst",out)
+        DYstline  = updateLine(line,"DYst",out)
+        TTXstline = updateLine(line,"TTXst",out)
+        Lumline   = updateLine(line,"Lum",out)
+        Trgline   = updateLine(line,"Trg",out)
+        JERline   = updateLine(line,"JER",out)
+        IDline    = updateLine(line,"ID",out)
+        ISOline   = updateLine(line,"ISO",out)
+
+        if not (Sstline or FastSline or VVstline or STstline or DYstline or TTXstline or Lumline or Trgline or JERline or IDline or ISOline): out.write(line)
+  
+  inp.close()
+  out.close()
+
