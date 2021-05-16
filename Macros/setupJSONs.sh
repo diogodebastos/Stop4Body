a0abd7afcff188972a4788eab89e7e7d53fa24d8
Diogo de Bastos <diogodebastos18@gmail.com>
2021-05-16 15:39:41 +0100

    update jsons

--- Macros/setupJSONs.sh
+++ Macros/setupJSONs.sh
@@ -132,6 +132,8 @@ elif [ ${YEAR} == 2016 ] || [ ${YEAR} == 2017 ] || [ ${YEAR} == 2018 ]; then
  mergeJSONs $JSON_PATH/allMC_LO.json                    $JSON_PATH/Orig/QCD.json $JSON_PATH/ttX.json $JSON_PATH/ZInv.json $JSON_PATH/Orig/VV.json $JSON_PATH/Orig/SingleTop.json $JSON_PATH/Orig/DYJets.json $JSON_PATH/TTbar.json $JSON_PATH/Wjets.json
  mergeJSONs $JSON_PATH/allMC-QCD-en.json             $JSON_PATH/QCD_en.json $JSON_PATH/ttX.json $JSON_PATH/ZInv.json $JSON_PATH/Orig/VV.json $JSON_PATH/Orig/SingleTop.json $JSON_PATH/Orig/DYJets.json $JSON_PATH/TTbar.json $JSON_PATH/Wjets.json
 
+ mergeJSONs $JSON_PATH/allMC_amcatnlo.json                    $JSON_PATH/Orig/QCD.json $JSON_PATH/ttX.json $JSON_PATH/ZInv.json $JSON_PATH/Orig/VV.json $JSON_PATH/Orig/SingleTop.json $JSON_PATH/Orig/DYJets.json $JSON_PATH/TTbar_amcatnlo.json $JSON_PATH/WJetsNLO.json
+
  mergeJSONs $JSON_PATH/otherMC2_QCD_Mu_en.json                 $JSON_PATH/Orig/SingleTop.json $JSON_PATH/Orig/QCD_Mu.json $JSON_PATH/Orig/TTGJets.json $JSON_PATH/Orig/TTW_LO.json $JSON_PATH/Orig/TTWToLNu.json $JSON_PATH/Orig/TTZToLLNuNu.json
 
  mergeJSONs $JSON_PATH/allMC_QCD_Mu_en.json                 $JSON_PATH/Orig/QCD_Mu.json $JSON_PATH/ttX.json $JSON_PATH/ZInv.json $JSON_PATH/Orig/VV.json $JSON_PATH/Orig/SingleTop.json $JSON_PATH/Orig/DYJets.json $JSON_PATH/TTbar.json $JSON_PATH/Wjets.json
@@ -252,6 +254,15 @@ elif [ ${YEAR} == 2016 ] || [ ${YEAR} == 2017 ] || [ ${YEAR} == 2018 ]; then
  mergeJSONs $JSON_PATH/plot${YEAR}_DM60RP.json     $JSON_PATH/allMC.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/Orig/SignalPoints/Sig_576_516.json
  mergeJSONs $JSON_PATH/plot${YEAR}_DM70RP.json     $JSON_PATH/allMC.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/Orig/SignalPoints/Sig_600_530.json
  mergeJSONs $JSON_PATH/plot${YEAR}_DM80RP.json     $JSON_PATH/allMC.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/Orig/SignalPoints/Sig_626_546.json
+
+ mergeJSONs $JSON_PATH/plot${YEAR}_DM10RP_amcatnlo.json     $JSON_PATH/allMC_amcatnlo.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/Orig/SignalPoints/Sig_475_465.json
+ mergeJSONs $JSON_PATH/plot${YEAR}_DM20RP_amcatnlo.json     $JSON_PATH/allMC_amcatnlo.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/Orig/SignalPoints/Sig_526_505.json
+ mergeJSONs $JSON_PATH/plot${YEAR}_DM30RP_amcatnlo.json     $JSON_PATH/allMC_amcatnlo.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/Orig/SignalPoints/Sig_550_520.json
+ mergeJSONs $JSON_PATH/plot${YEAR}_DM40RP_amcatnlo.json     $JSON_PATH/allMC_amcatnlo.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/Orig/SignalPoints/Sig_576_536.json
+ mergeJSONs $JSON_PATH/plot${YEAR}_DM50RP_amcatnlo.json     $JSON_PATH/allMC_amcatnlo.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/Orig/SignalPoints/Sig_576_526.json
+ mergeJSONs $JSON_PATH/plot${YEAR}_DM60RP_amcatnlo.json     $JSON_PATH/allMC_amcatnlo.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/Orig/SignalPoints/Sig_576_516.json
+ mergeJSONs $JSON_PATH/plot${YEAR}_DM70RP_amcatnlo.json     $JSON_PATH/allMC_amcatnlo.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/Orig/SignalPoints/Sig_600_530.json
+ mergeJSONs $JSON_PATH/plot${YEAR}_DM80RP_amcatnlo.json     $JSON_PATH/allMC_amcatnlo.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/Orig/SignalPoints/Sig_626_546.json
 
  mergeJSONs $JSON_PATH/allDM10.json $JSON_PATH/allMC.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/stop_deltaM10.json
  mergeJSONs $JSON_PATH/allDM20.json $JSON_PATH/allMC.json $JSON_PATH/Orig/DataMetHT.json $JSON_PATH/stop_deltaM20.json
