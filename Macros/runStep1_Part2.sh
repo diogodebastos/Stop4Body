47871caf57f471dfe2171c31ee803e2410525256
Diogo de Bastos <diogodebastos18@gmail.com>
2021-07-02 19:55:08 +0100

    TTPOW+WJETS train test samples

--- Macros/runStep1_Part2.sh
+++ Macros/runStep1_Part2.sh
@@ -5,6 +5,7 @@
 
 cd Step1Scripts
 
-qsub -q lipq -v CMSSW_BASE=$CMSSW_BASE Step1_Part2.sh
+#qsub -q lipq -v CMSSW_BASE=$CMSSW_BASE Step1_Part2.sh
+sbatch -p lipq --mem=2500 Step1_Part2.sh --export=CMSSW_BASE=$CMSSW_BASE
 
 cd -
